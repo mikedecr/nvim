f06beff2a4de9e0ff02f566d168e02d1fec81edc
Michael DeCrescenzo <mgdecrescenzo@gmail.com>
2022-06-02 00:01:42 -0500

    lua configs

--- README.md
+++ README.md
@@ -1,5 +1,12 @@
 # Neovim Configs
 
+## This is the LUA BRANCH
+
+We are experimenting with lua-driven configs.
+
+
+## Repo details
+
 This repo is managed out of `$HOME/.config/nvim`.
 It does not need to be a bare repo---we keep no files in `$HOME` (i.e. we bypass `.vimrc` entirely) at least for now.
 
---
+++ lua/user/options.lua
@@ -0,0 +1,72 @@
+-- Neovim from Scratch:
+-- https://www.youtube.com/watch?v=hY5-Q6NxQgY&list=PLhoH5vyxr6Qq41NFL4GvhFp-WLd5xzIzZ&index=2
+
+vo = vim.opt
+
+-- move me
+-- vo.shortmess:append "c"
+
+-- menus, modes
+vo.showmode = true                         -- we don't need to see things like -- INSERT -- anymore
+vo.cmdheight = 1                           -- more space in the neovim command line for displaying messages
+vo.pumheight = 10                          -- pop up menu height
+
+-- file behavior
+vo.fileencoding = "utf-8"                  -- the encoding written to a file
+vo.backup = false                          -- creates a backup file
+vo.writebackup = false                     -- if file is being edited by another program (or was written while editing with another program), it is not allowed to be edited
+vo.swapfile = false                        -- creates a swapfile
+
+-- search
+vo.ignorecase = true                       -- ignore case in search patterns
+vo.smartcase = true                        -- smart case
+vo.hlsearch = true                         -- highlight all matches on previous search pattern
+
+-- buffers / splits
+vo.title = true
+vo.splitbelow = true                       -- force all horizontal splits to go below current window
+vo.splitright = true                      -- force all vertical splits to go to the right of current window
+-- vo.showtabline = 2                      -- always show tabs
+
+-- lines
+vo.cursorline = true                       -- highlight the current line
+vo.number = true
+vo.relativenumber = true
+
+-- gutter
+vo.signcolumn = "yes"                      -- always show the sign column, otherwise it would shift the text each time
+vo.numberwidth = 3                         -- set number column width to 2 {default 4}
+
+-- tabstop
+vo.shiftwidth = 4                          -- the number of spaces inserted for each indentation
+vo.tabstop = 4                             -- insert n spaces for a tab
+vo.expandtab = true                        -- convert tabs to spaces
+vo.smarttab = true
+vo.smartindent = true                      -- make indenting smarter again
+vo.autoindent = true
+
+-- wrap
+vo.wrap = false                            -- display lines as one long line
+vim.cmd "set whichwrap+=<,>,[,],h,l"
+vim.cmd "set backspace=indent,eol,start"
+
+-- scrolling / scanning
+vo.scrolloff = 4
+vo.sidescrolloff = 8
+
+-- interactivity
+vo.mouse = "a"
+-- vo.paste = true 			               -- setting paste in config creates problems??
+vo.clipboard = "unnamedplus"
+
+-- maps and completions
+vo.timeoutlen = 1000                       -- time to wait for a mapped sequence to complete (in milliseconds)
+vo.updatetime = 300                        -- faster completion (4000ms default)
+
+-- undo
+vo.undodir = ".undo_dir"
+vo.undofile = true
+
+-- fonts
+vim.cmd "highlight Comment cterm=italic gui=italic"
+
