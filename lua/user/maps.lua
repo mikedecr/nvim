7ba0db6153e4f0b57d74a012336d5360268d5916
Michael DeCrescenzo <mdecrescenzo@drwholdings.com>
2022-06-22 17:29:40 -0500

    prevent [leader]f clash w/ vimcmdline

--- lua/user/maps.lua
+++ lua/user/maps.lua
@@ -12,7 +12,7 @@ local opts = {noremap = true, silent = true}
 -- ::: NORMAL :::
 
 -- telescope
-keymap("n", "<leader>f", "<cmd>lua require'telescope.builtin'.find_files(require('telescope.themes').get_dropdown({ previewer = false }))<cr>", opts)
+keymap("n", "<leader>ff", "<cmd>lua require'telescope.builtin'.find_files(require('telescope.themes').get_dropdown({ previewer = false }))<cr>", opts)
 keymap("n", "<c-t>", "<cmd>Telescope live_grep<cr>", opts)
 
 -----------------------------------------------------
