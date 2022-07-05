66b650f19f3f43c3b38cc56c5b2f50b38ee6f008
Michael DeCrescenzo <mdecrescenzo@drwholdings.com>
2022-07-05 17:40:08 -0500

    reserve C-k for navigating splits

--- lua/user/maps.lua
+++ lua/user/maps.lua
@@ -11,6 +11,12 @@ local opts = {noremap = true, silent = true}
 -----------------------------------------------------
 -- ::: NORMAL :::
 
+-- Better window navigation
+keymap("n", "<C-h>", "<C-w>h", opts)
+keymap("n", "<C-j>", "<C-w>j", opts)
+keymap("n", "<C-k>", "<C-w>k", opts)
+keymap("n", "<C-l>", "<C-w>l", opts)
+
 -- telescope
 keymap("n", "<leader>ff", "<cmd>lua require'telescope.builtin'.find_files(require('telescope.themes').get_dropdown({ previewer = false }))<cr>", opts)
 keymap("n", "<c-t>", "<cmd>Telescope live_grep<cr>", opts)
