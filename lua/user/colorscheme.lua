5261e66fb2d35f9a085d8f86a1b7d3218547f36e
Michael DeCrescenzo <mgdecrescenzo@gmail.com>
2022-07-09 15:46:41 -0500

    add other optional schemes

--- lua/user/colorscheme.lua
+++ lua/user/colorscheme.lua
@@ -1,12 +1,20 @@
 local scheme = "nord"
+-- local scheme = "everforest"
+-- local scheme = "fairyfloss"
+-- local scheme = "peachpuff"
 
 -- scheme-specific settings.
 -- activated if you load the scheme from `colorscheme [name]`
 
+-- nord
 vim.g.nord_borders = true     -- vertical split border
 vim.g.nord_contrast = true    -- popups/bars different background
 vim.g.nord_italic = false     -- italic flourishes
 
+-- everforest
+if scheme == "everforest" then
+    background = light
+end
 
 -- .set() the theme
 -- scheme_ok describes state of the pcall
