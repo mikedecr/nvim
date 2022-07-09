c150365f1512c4c571d0becabd5544c86c3571cf
Michael DeCrescenzo <mgdecrescenzo@gmail.com>
2022-07-09 13:23:25 -0500

    typo

--- lua/user/plug.lua
+++ lua/user/plug.lua
@@ -44,7 +44,7 @@ return packer.startup(function(use)
   -- ::: packer
   use "wbthomason/packer.nvim" -- Have packer manage itself
   use "nvim-lua/popup.nvim"    -- An implementation of the Popup API from vim in Neovim
-  use "nvim-lua/plenary.nvim"  -- Useful lua functions used ny lots of plugins
+  use "nvim-lua/plenary.nvim"  -- Useful lua functions used by lots of plugins
 
   -- ::: general
   use "rcarriga/nvim-notify"
