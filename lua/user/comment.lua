35d80a37b1c1cda562ec16ca697e9c32a546b1d8
Michael DeCrescenzo <mgdecrescenzo@gmail.com>
2022-07-28 21:52:32 -0500

    cleanup

--- lua/user/comment.lua
+++ lua/user/comment.lua
@@ -1,13 +1,9 @@
 local ok, com = pcall(require, 'Comment')
 
-if ok then com.setup()
-else vim.notify("comment.nvim fails") end
-
---[[ if not ok then
-    vim.notify("comment.nvim fails")
+if not okay
+    then vim.notify("comment.nvim fails")
     return
-else
-    com.setup()
-end ]]
+end
 
+com.setup()
 
