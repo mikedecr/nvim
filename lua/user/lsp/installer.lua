79e42f51841c37f34e8396fb311778ba70abc66f
Michael DeCrescenzo <mgdecrescenzo@gmail.com>
2022-08-20 14:39:47 -0500

    julia server

--- lua/user/lsp/installer.lua
+++ lua/user/lsp/installer.lua
@@ -24,6 +24,10 @@ lsp_installer.on_server_ready(function(server)
 	 	opts = vim.tbl_deep_extend("force", pyright_opts, opts)
 	end
 
+    if server.name == "julials" then
+        local jl_ok, julials_opts = pcall(require, "user.lsp.settings.julials")
+    end
+
 	-- This setup() function is exactly the same as lspconfig's setup function.
 	-- Refer to https://github.com/neovim/nvim-lspconfig/blob/master/doc/server_configurations.md
 	server:setup(opts)
