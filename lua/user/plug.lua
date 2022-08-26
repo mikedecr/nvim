557b077410619111eb12d17c105d7e3c26a13ad6
Michael DeCrescenzo <mdecrescenzo@drwholdings.com>
2022-08-26 16:34:50 -0500

    reset plug save autocmd

--- lua/user/plug.lua
+++ lua/user/plug.lua
@@ -14,12 +14,12 @@ end
 
 
 -- -- Autocommand that reloads neovim whenever you save the plugins.lua file
--- vim.cmd [[
---     augroup packer_user_config
---         autocmd!
---         autocmd BufWritePost plug.lua source <afile> | PackerSync
---     augroup end
--- ]]
+vim.cmd [[
+    augroup packer_user_config
+        autocmd!
+        autocmd BufWritePost plug.lua source <afile> | PackerSync
+    augroup end
+]]
 
 
 -- Use a protected call so we don't error out on first use
