98e17e897a2e4bca6feb2558940d87833612bef4
Michael DeCrescenzo <mgdecrescenzo@gmail.com>
2022-09-10 09:31:24 -0500

    notify msg

--- lua/user/lsp/init.lua
+++ lua/user/lsp/init.lua
@@ -1,5 +1,8 @@
 local lsp_ok, _ = pcall(require, "lspconfig")
-if not lsp_ok then return end
+if not lsp_ok then 
+    vim.notify("require 'lspconfig' fails")
+    return
+end
 
 require("user.lsp.installer")
 require("user.lsp.handlers").setup()
