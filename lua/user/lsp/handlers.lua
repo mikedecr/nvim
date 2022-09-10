4ce80fd088f61ee6632fd76f55022a0163a719ac
Michael DeCrescenzo <mgdecrescenzo@gmail.com>
2022-09-10 09:31:33 -0500

    fix indents

--- lua/user/lsp/handlers.lua
+++ lua/user/lsp/handlers.lua
@@ -3,49 +3,52 @@
 -- M is a namespace for placing exportable objects
 local M = {}
 
+-- .setup()
 -- TODO: backfill this to template
 M.setup = function()
-  local signs = {
-    { name = "DiagnosticSignError", text = "" },
-    { name = "DiagnosticSignWarn", text = "" },
-    { name = "DiagnosticSignHint", text = "" },
-    { name = "DiagnosticSignInfo", text = "" },
-  }
 
-  for _, sign in ipairs(signs) do
-    vim.fn.sign_define(sign.name, { texthl = sign.name, text = sign.text, numhl = "" })
-  end
+    -- signs that appear on LSP messages
+    local signs = {
+        { name = "DiagnosticSignError", text = "" },
+        { name = "DiagnosticSignWarn", text = "" },
+        { name = "DiagnosticSignHint", text = "" },
+        { name = "DiagnosticSignInfo", text = "" },
+    }
 
-  local config = {
-    -- disable virtual text
-    virtual_text = false,
-    -- show signs
-    signs = {
-      active = signs,
-    },
-    update_in_insert = true,
-    underline = true,
-    severity_sort = true,
-    float = {
-      focusable = false,
-      style = "minimal",
-      border = "rounded",
-      source = "always",
-      header = "",
-      prefix = "",
-    },
-  }
+    -- assign local `signs` data to vim
+    for _, sign in ipairs(signs) do
+        vim.fn.sign_define(sign.name, { texthl = sign.name, text = sign.text, numhl = "" })
+    end
 
-  vim.diagnostic.config(config)
+    local config = {
+        -- disable virtual text
+        virtual_text = false,
+        -- show signs
+        signs = {
+            active = signs,
+        },
+        update_in_insert = true,
+        underline = true,
+        severity_sort = true,
+        float = {
+            focusable = false,
+            style = "minimal",
+            border = "rounded",
+            source = "always",
+            header = "",
+            prefix = "",
+        },
+    }
 
-  vim.lsp.handlers["textDocument/hover"] = vim.lsp.with(vim.lsp.handlers.hover, {
-    border = "rounded",
-  })
+    vim.diagnostic.config(config)
 
-  vim.lsp.handlers["textDocument/signatureHelp"] = vim.lsp.with(vim.lsp.handlers.signature_help, {
-    border = "rounded",
-  })
+    vim.lsp.handlers["textDocument/hover"] = vim.lsp.with( vim.lsp.handlers.hover,
+                                                           { border = "rounded", } )
+    vim.lsp.handlers["textDocument/signatureHelp"] = vim.lsp.with(vim.lsp.handlers.signature_help, 
+                                                                  { border = "rounded", })
 end
+
+-- ::: LOCAL FUNCTIONS
 
 -- highlight entity on hover
 local function lsp_highlight_document(client)
@@ -92,9 +95,10 @@ local capabilities = vim.lsp.protocol.make_client_capabilities()
 local status_ok, cmp_nvim_lsp = pcall(require, "cmp_nvim_lsp")
 if not status_ok then 
     vim.notify("cmp_nvim_lsp failed")
-    return 
+    return
 end
 
 M.capabilities = cmp_nvim_lsp.update_capabilities(capabilities)
 
 return M
+
