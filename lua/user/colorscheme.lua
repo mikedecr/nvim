702293da910f95e14b1948c4f2d2d0db747c8f95
Michael DeCrescenzo <mgdecrescenzo@gmail.com>
2022-11-05 17:24:05 -0500

    settings

--- lua/user/colorscheme.lua
+++ lua/user/colorscheme.lua
@@ -17,13 +17,14 @@ if scheme == "nord" then
     vim.g.nord_borders = true     -- vertical split border
     vim.g.nord_contrast = true    -- popups/bars different background
     vim.g.nord_italic = false     -- italic flourishes
+    vim.g.nord_bold = false
 end
 
 if scheme == "nordic" then
     vim.g.nord_underline_option = 'none'
     vim.g.nord_italic = false
     vim.g.nord_italic_comments = true
-    vim.g.nord_minimal_mode = true
+    vim.g.nord_minimal_mode = false
     vim.g.nord_alternate_backgrounds = true
 end
 
@@ -32,5 +33,8 @@ end
 -- scheme_ok describes state of the pcall
 -- `_` is returned value from the call (in this case, we don't care what the value is)
 local scheme_ok, _ = pcall(vim.cmd, "colorscheme " .. scheme)
-if not scheme_ok then vim.notify("colorscheme " .. scheme .. " not found!") return end
+if not scheme_ok then
+    vim.notify("colorscheme " .. scheme .. " not found!")
+    return
+end
 
