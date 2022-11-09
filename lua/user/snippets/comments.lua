1c1a06a369e70ba24af0a5ab3448de3ac8d9d6cb
Michael DeCrescenzo <mdecrescenzo@drwholdings.com>
2022-11-09 13:07:44 -0600

    trim noop stuff out of comments

--- lua/user/snippets/comments.lua
+++ lua/user/snippets/comments.lua
@@ -1,8 +1,5 @@
 local snip_ok, ls = pcall(require, "luasnip")
-local calc_ok, calc_comment_str = require("Comment.ft").calculate
-local utils_ok, utils = require("Comment.utils")
-
-if not (snip_ok and calc_ok and utils_ok) then
+if not snip_ok then
     vim.notify("LuaSnip and Comments can't be used to write comment snippets")
     return
 end
@@ -10,22 +7,6 @@ end
 local text = ls.text_node
 local snip = ls.snippet
 local insert = ls.insert_node
-
-
--- --- Get the comment string {beg,end} table
--- ---@param ctype integer 1 for `line`-comment and 2 for `block`-comment
--- ---@return table comment_strings {begcstring, endcstring}
--- local get_cstring = function(ctype)
---     -- use `Comments.nvim` API to fetch comment str for the region
---     -- (eq. '--%s' or '--[[%s]]' for `lua`)
---     local cstring = calc_comment_str { ctype = ctype, range = utils.get_region() } or vim.bo.commentstring
---     -- as we want only the strings themselves and not strings ready for using `format`
---     -- we want to split the left and right side
---     local left, right = utils.unwrap_cstr(cstring)
---     -- either parts can be nil so we optionally return empty strings
---     -- while creating a two-element table
---     return { left or '', right or '' }
--- end
 
 
 local dashes = function(n)
