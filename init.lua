7e20c95f3d80e534a79595250e8ef649477a61af
Michael DeCrescenzo <mgdecrescenzo@gmail.com>
2023-01-17 07:38:57 -0600

    cleanupo init

--- init.lua
+++ init.lua
@@ -12,31 +12,9 @@ require "user.maps"          -- keymaps
 -- :: thematic setup ::
 require "user.visual"
 require "user.navigation"
-
--- text UI
-require "user.snippets"      -- configure and write snippets
-require "user.cmp"           -- completion (source registry, keymaps, menu appearances)
-require "user.autopairs"     -- self-explanatory
-require "user.surround"      -- sandwich/surround with characters
-require "user.neoclip"       -- yank/macro history
-require "user.iron"          -- cross-language REPL
--- require "user.comment"       -- comment controls
-
--- syntax
-require "user.treesitter"    -- syntax improvements
-require "user.yati"          -- patch indentation for treesitter
-require "user.pandoc"
--- require "user.headlines"     -- prettier markdown chunks etc.
+require "user.ui" -- for "text interface" only, what neovim does to you when you type
+require "user.syntax"
 
 -- lsp
 -- require "user.lsp"           -- language servers (this is a dir)
-
-
--- TODO: this is ALMOST good.
-
--- missing:
--- nui
--- stan
--- pandoc
--- quarto
 
