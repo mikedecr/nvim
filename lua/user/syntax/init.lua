ca1e7270825de0feb30056102511ae6f3212f5ce
Michael DeCrescenzo <mgdecrescenzo@gmail.com>
2023-01-17 07:37:24 -0600

    [syntax] init syntax folder

---
+++ lua/user/syntax/init.lua
@@ -0,0 +1,12 @@
+
+local syntax = 'user.syntax.'
+
+require(syntax .. 'autopairs')
+require(syntax .. 'cmp')
+require(syntax .. 'comment')
+require(syntax .. 'headlines')
+require(syntax .. 'pandoc')
+require(syntax .. 'surround')
+require(syntax .. 'treesitter')
+require(syntax .. 'yati')
+
