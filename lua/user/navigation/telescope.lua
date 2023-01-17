269882ad191baa7f23105adf308d96feaedce7ca
Michael DeCrescenzo <mgdecrescenzo@gmail.com>
2023-01-16 20:53:40 -0600

    reorder telescope keymaps

--- lua/user/navigation/telescope.lua
+++ lua/user/navigation/telescope.lua
@@ -1,43 +1,62 @@
-local telescope_ok, telescope = pcall(require, "telescope")
-if not telescope_ok then return end
+-- :::::::::::::::::::
+-- :::: telescope ::::
+-- :::::::::::::::::::
 
--- telescope.load_extension('media_files')
+-- extensible fuzzy finder
 
-local actions = require "telescope.actions"
+local tele_ok, telescope = pcall(require, "telescope")
+if not tele_ok then vim.notify('failed: telescope'); return; end
+
+local actions_ok, actions = pcall(require, 'telescope.actions')
+if not actions_ok then vim.notify('failed: telescope.actions'); return; end
+
 
 telescope.setup {
   defaults = {
 
+    -- Visual display of the picker
+    pickers = {
+        find_files =  { theme = "ivy" },
+        oldfiles =    { theme = "dropdown" },
+        projects =    { theme = "dropdown" },
+        live_grep =   { theme = "ivy" },
+        git_commits = { theme = "ivy" }
+    },
     prompt_prefix = "? > ",
     selection_caret = "-> ",
     path_display = { "smart" },
 
+    -- keymaps by mode
     mappings = {
       i = {
         ["<C-n>"] = actions.cycle_history_next,
         ["<C-p>"] = actions.cycle_history_prev,
 
+        -- navigate search list
         ["<C-j>"] = actions.move_selection_next,
         ["<C-k>"] = actions.move_selection_previous,
-
-        ["<C-c>"] = actions.close,
-
         ["<Down>"] = actions.move_selection_next,
         ["<Up>"] = actions.move_selection_previous,
+        ["<PageUp>"] = actions.results_scrolling_up,
+        ["<PageDown>"] = actions.results_scrolling_down,
 
+        -- toggle selections (not entirely sure I understand...)
+        ["<Tab>"] = actions.toggle_selection + actions.move_selection_worse,
+        ["<S-Tab>"] = actions.toggle_selection + actions.move_selection_better,
+
+        -- selection from / exit search list
+        ["<C-c>"] = actions.close,
         ["<CR>"] = actions.select_default,
+        -- select into a buffer split, tab, etc.
         ["<C-x>"] = actions.select_horizontal,
         ["<C-v>"] = actions.select_vertical,
         ["<C-t>"] = actions.select_tab,
 
+        -- preview buffer
         ["<C-u>"] = actions.preview_scrolling_up,
         ["<C-d>"] = actions.preview_scrolling_down,
 
-        ["<PageUp>"] = actions.results_scrolling_up,
-        ["<PageDown>"] = actions.results_scrolling_down,
-
-        ["<Tab>"] = actions.toggle_selection + actions.move_selection_worse,
-        ["<S-Tab>"] = actions.toggle_selection + actions.move_selection_better,
+        -- quick fix list etc.
         ["<C-q>"] = actions.send_to_qflist + actions.open_qflist,
         ["<M-q>"] = actions.send_selected_to_qflist + actions.open_qflist,
         ["<C-l>"] = actions.complete_tag,
@@ -45,64 +64,67 @@ telescope.setup {
       },
 
       n = {
+
+        -- navigate 
+        ["j"] = actions.move_selection_next,
+        ["k"] = actions.move_selection_previous,
+        ["<Down>"] = actions.move_selection_next,
+        ["<Up>"] = actions.move_selection_previous,
+        ["gg"] = actions.move_to_top,
+        ["G"] = actions.move_to_bottom,
+        ["<PageUp>"] = actions.results_scrolling_up,
+        ["<PageDown>"] = actions.results_scrolling_down,
+
+        -- preview buffer
+        ["<C-u>"] = actions.preview_scrolling_up,
+        ["<C-d>"] = actions.preview_scrolling_down,
+
+        -- selection / exit
         ["<esc>"] = actions.close,
         ["<CR>"] = actions.select_default,
         ["<C-x>"] = actions.select_horizontal,
         ["<C-v>"] = actions.select_vertical,
         ["<C-t>"] = actions.select_tab,
 
+        -- toggle selection
         ["<Tab>"] = actions.toggle_selection + actions.move_selection_worse,
         ["<S-Tab>"] = actions.toggle_selection + actions.move_selection_better,
+
+        -- qfix list etc
         ["<C-q>"] = actions.send_to_qflist + actions.open_qflist,
         ["<M-q>"] = actions.send_selected_to_qflist + actions.open_qflist,
-
-        ["j"] = actions.move_selection_next,
-        ["k"] = actions.move_selection_previous,
+        ["?"] = actions.which_key,
+                --
+        -- drag options
         ["H"] = actions.move_to_top,
         ["M"] = actions.move_to_middle,
         ["L"] = actions.move_to_bottom,
 
-        ["<Down>"] = actions.move_selection_next,
-        ["<Up>"] = actions.move_selection_previous,
-        ["gg"] = actions.move_to_top,
-        ["G"] = actions.move_to_bottom,
-
-        ["<C-u>"] = actions.preview_scrolling_up,
-        ["<C-d>"] = actions.preview_scrolling_down,
-
-        ["<PageUp>"] = actions.results_scrolling_up,
-        ["<PageDown>"] = actions.results_scrolling_down,
-
-        ["?"] = actions.which_key,
       },
     },
   },
-  pickers = {
-    find_files = { theme = "ivy" },
-    oldfiles = { theme = "dropdown" },
-    projects = { theme = "dropdown" },
-    live_grep = { theme = "ivy" },
-    git_commits = { theme = "ivy" }
-    -- Default configuration for builtin pickers goes here:
-    -- picker_name = {
-    --   picker_config_key = value,
-    --   ...
-    -- }
-    -- Now the picker_config_key will be applied every time you call this
-    -- builtin picker
-  },
-  extensions = {
-    media_files = {
-        -- filetypes whitelist
-        -- defaults to {"png", "jpg", "mp4", "webm", "pdf"}
-        filetypes = {"png", "webp", "jpg", "jpeg"},
-        find_cmd = {"rg", "-L"} -- find command (defaults to `fd`)
-      }
-    -- Your extension configuration goes here:
-    -- extension_name = {
-    --   extension_config_key = value,
-    -- }
-    -- please take a look at the readme of the extension you want to configure
-  },
+
 }
+
+-- ::: setup media files :::
+
+telescope.load_extension('media_files')
+local media_files_setup = {
+    -- filetypes whitelist
+    -- defaults to {"png", "jpg", "mp4", "webm", "pdf"}
+    filetypes = {"png", "webp", "jpg", "jpeg"},
+    find_cmd = {"rg", "-L"} -- find command (defaults to `fd`)
+}
+
+-- Your extension configuration goes here:
+-- extension_name = {
+--   extension_config_key = value,
+-- }
+-- see readme for your desired extension
+telescope.setup {
+  extensions = {
+    media_files = media_files_setup,
+  }
+}
+
 
