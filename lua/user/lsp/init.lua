482251bc69bc643650bf692ce826a8a313299ef2
Michael DeCrescenzo <mgdecrescenzo@gmail.com>
2023-01-21 10:19:59 -0600

    neodev enhances lua LS in nvim configs

--- lua/user/lsp/init.lua
+++ lua/user/lsp/init.lua
@@ -2,6 +2,7 @@
 
 local lsp = 'user.lsp.'
 
+require(lsp .. "before")            -- better lua support in nvim config
 require(lsp .. "lsp_zero")          -- easy LSP install/hookup
 require(lsp .. "client_settings")   -- server-specific
 
