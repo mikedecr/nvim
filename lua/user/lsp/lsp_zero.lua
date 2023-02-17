6e8a8af924a3fa178635e2395ef62f713b0dc63a
Michael DeCrescenzo <mdecrescenzo@drwholdings.com>
2023-02-17 10:51:50 -0600

    fix broken LSP keymaps thanks to upstream

--- lua/user/lsp/lsp_zero.lua
+++ lua/user/lsp/lsp_zero.lua
@@ -1,6 +1,12 @@
 local ok, lsp = pcall(require, 'lsp-zero')
 if not ok then vim.notify('failed: lsp-zero'); return; end
 
-lsp.preset('recommended')
+lsp.preset({
+    name = 'recommended',
+    set_lsp_keymaps = {
+        preserve_mappings = false -- temp until you define your own lsp maps
+    },
+    suggest_lsp_servers = true,
+})
 lsp.setup()
 
