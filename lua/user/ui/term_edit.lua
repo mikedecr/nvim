22797f1ebbdf409309138e10b0ab0c1a62f81bea
Michael DeCrescenzo <mgdecrescenzo@gmail.com>
2023-03-01 20:56:12 -0600

    comments

--- lua/user/ui/term_edit.lua
+++ lua/user/ui/term_edit.lua
@@ -1,6 +1,8 @@
 ok, term_edit = pcall(require, "term-edit")
 if not ok then vim.notify("failed: term-edit"); return; end
 
+-- these need to be kept up to date w/ starship prompt configuration
+-- perhaps we could read from that file somehow...
 term_edit.setup({
     prompt_end = {"❯", "❮"}
 })
