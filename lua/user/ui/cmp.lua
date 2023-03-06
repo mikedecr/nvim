dcefc272685d3ecf5424acc97f79cfd65e9c89af
Michael DeCrescenzo <mgdecrescenzo@gmail.com>
2023-03-05 22:57:23 -0600

    cleanup cmp file

--- lua/user/ui/cmp.lua
+++ lua/user/ui/cmp.lua
@@ -1,26 +1,28 @@
 -- video tutorial: https://www.youtube.com/watch?v=_DnmphIwnjo&t=523s
 
-
 local cmp_status_ok, cmp = pcall(require, "cmp")
-if not cmp_status_ok then 
-    vim.notify('failed: cmp')
-    return
-end
+if not cmp_status_ok then vim.notify('failed: cmp') return end
+
+
+-- ::::::::::::::::::::::::::::::::::::::::::::::::::
+-- :::: injection function from a snippet engine ::::
+-- ::::::::::::::::::::::::::::::::::::::::::::::::::
 
 local snip_status_ok, luasnip = pcall(require, "luasnip")
-if not snip_status_ok then
-    vim.notify('failed: luasnip')
-    return
-end
+if not snip_status_ok then vim.notify('failed: luasnip') return end
 
--- expanding function for `luasnip` engine.
+-- our function
 local expand_snippets = function(args) luasnip.lsp_expand(args.body) end
 
+-- idk what you do but I'm too afraid to ask
 require("luasnip/loaders/from_vscode").lazy_load()
 
 
----- map controls ---------
+-- :::::::::::::::::
+-- :::: keymaps ::::
+-- :::::::::::::::::
 
+-- used for Tab key
 local check_backspace = function()
   local col = vim.fn.col "." - 1
   return col == 0 or vim.fn.getline("."):sub(col, col):match "%s"
@@ -67,76 +69,99 @@ local maps = {
   }
 
 
+-- ::::::::::::::::::::::::::::::::::::::::::::::::
+-- :::: key-value for completion item "kinds"  ::::
+-- ::::::::::::::::::::::::::::::::::::::::::::::::
+
 --   פּ ﯟ   some other good icons
 -- find more here: https://www.nerdfonts.com/cheat-sheets
+
+-- these are LSP objects?
+-- and the icons are from vscode?
 local kind_icons = {
-  Text = "",
-  Method = "m",
-  Function = "",
-  Constructor = "",
-  Field = "",
-  Variable = "",
-  Class = "",
-  Interface = "",
-  Module = "",
-  Property = "",
-  Unit = "",
-  Value = "",
-  Enum = "",
-  Keyword = "",
-  Snippet = "",
-  Color = "",
-  File = "",
-  Reference = "",
-  Folder = "",
-  EnumMember = "",
-  Constant = "",
-  Struct = "",
-  Event = "",
-  Operator = "",
-  TypeParameter = "",
+    -- stuff that might matter to me
+    File = "",
+    Folder = "",
+    Value = "",
+    Text = "",
+    Method = "m",
+    Function = "",
+    Keyword = "",
+    Field = "",
+    Variable = "",
+    Constant = "",
+    Class = "",
+    Module = "",
+    Operator = "",
+    Snippet = "",
+    -- less likely
+    Interface = "",
+    Struct = "",
+    Constructor = "",
+    Reference = "",
+    Property = "",
+    Enum = "",
+    EnumMember = "",
+    TypeParameter = "",
+    Event = "",
+    Unit = "",
+    Color = "",
 }
 
+-- :::::::::::::::::::::
+-- :::: Source data ::::
+-- :::::::::::::::::::::
 
-cmp.setup {
-  -- we need a snippet engine
-  snippet = { expand = expand_snippets },
-  -- ordering affects search priority of snippet sources
-  sources = {
-    { name = "luasnip" },
-    { name = "nvim_lsp" },
-    { name = "nvim_lua" },
-    { name = "path" },
-    { name = "buffer", keyword_length = 5 },
+-- TODO: we want this to be a table of name, menu value, and opts to unpack?
+-- local sources_menu = {
+--     luasnip = "[Snippet]", 
+--     nvim_lsp = "[LSP]",
+--     nvim_lua = "[NVIM_LUA]",
+--     path = "[Path]",
+--     buffer = "[Buffer]",
+-- }
+
+
+-- :::::::::::::::::::::::::::::
+-- :::: Final configuration ::::
+-- :::::::::::::::::::::::::::::
+
+cmp.setup({
+
+    -- source data and snippet functionality
+    -- ordering affects search priority
+    sources = {
+        { name = "luasnip" },
+        { name = "nvim_lsp" },
+        { name = "nvim_lua" },
+        { name = "path" },
+        { name = "buffer", keyword_length = 5 },
+    },
+    snippet = { expand = expand_snippets },
+
+    -- UI
+    mapping = maps,
+    confirm_opts = { behavior = cmp.ConfirmBehavior.Replace
+                   , select = false },
+
+    -- aesthetics of completion menu
+    formatting = {
+        fields = { "kind", "abbr", "menu" }, -- left -> right order
+        -- TODO: we can pull the src-by-src stuff out of here, 
+        --       don't hard code the table etc.
+        format = function(entry, vim_item)
+            -- kind: lookup icon from the table
+            vim_item.kind = string.format("%s", kind_icons[vim_item.kind])
+            -- This concatonates the icons with the name of the item kind
+            -- vim_item.kind = string.format('%s %s', kind_icons[vim_item.kind], vim_item.kind)
+            -- source menu item: lookup from the following
+            vim_item.menu = ({ nvim_lsp = "[LSP]",
+                               nvim_lua = "[NVIM_LUA]",
+                               luasnip = "[Snippet]",
+                               path = "[Path]",
+                               buffer = "[Buffer]"})[entry.source.name]
+            return vim_item
+        end,
   },
-  mapping = maps,
-  confirm_opts = { behavior = cmp.ConfirmBehavior.Replace
-                 , select = false },
-  -- ::: controls the appearance of the snippet menu
-  formatting = {
-    fields = { "kind", "abbr", "menu" },
-    format = function(entry, vim_item)
-      -- Kind icons
-      vim_item.kind = string.format("%s", kind_icons[vim_item.kind])
-      -- vim_item.kind = string.format('%s %s', kind_icons[vim_item.kind], vim_item.kind) -- This concatonates the icons with the name of the item kind
-      vim_item.menu = ({
-        nvim_lsp = "[LSP]",
-        nvim_lua = "[NVIM_LUA]",
-        luasnip = "[Snippet]",
-        path = "[Path]",
-        buffer = "[Buffer]",
-      })[entry.source.name]
-      return vim_item
-    end,
-  },
-  window = {
-    documentation = {
-        border = { "╭", "─", "╮", "│", "╯", "─", "╰", "│" },
-    }
-  },
-  experimental = {
-    ghost_text = true,
-    native_menu = false,
-  },
-}
+})
 
