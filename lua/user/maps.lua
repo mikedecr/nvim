f66f1c776b67fd1e26cf70818f6b91e87a2b40d0
Michael DeCrescenzo <mgdecrescenzo@gmail.com>
2023-03-05 20:59:59 -0600

    stole a few prim*agen maps

--- lua/user/maps.lua
+++ lua/user/maps.lua
@@ -19,10 +19,24 @@ keymap("n", "<C-j>", "<C-w>j", opts)
 keymap("n", "<C-k>", "<C-w>k", opts) -- this conflicts w/ LSP somehow...
 keymap("n", "<C-l>", "<C-w>l", opts)
 
+-- J without moving cursor to EOL
+keymap("n", "J", "mzJ`z", opts)
+
+-- normal half-page jumping, but recenter the cursor
+vim.keymap.set("n", "<C-d>", "<C-d>zz")
+vim.keymap.set("n", "<C-u>", "<C-u>zz")
+
+
 -- select rmd/qmd chunk
 -- TODO maybe relocate to a writing / markdown config?
 keymap("n", "vic", "/```<cr>NjVnk", opts) -- select chunk contents
 keymap("n", "vac", "/```<cr>NVn", opts)   -- select chunk + delimiters
+
+-- quickfix list navigation
+vim.keymap.set("n", "<C-Q><C-j>", "<cmd>cnext<CR>zz")
+vim.keymap.set("n", "<C-Q><C-k>", "<cmd>cprev<CR>zz")
+vim.keymap.set("n", "<C-Q><C-n>", "<cmd>lnext<CR>zz")
+vim.keymap.set("n", "<C-Q><C-p>", "<cmd>lprev<CR>zz")
 
 
 -- :::::::::::::::::::::
