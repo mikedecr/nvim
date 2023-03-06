aed3031357314390e844d3a6f264fba2f0cfe27d
Michael DeCrescenzo <mgdecrescenzo@gmail.com>
2023-03-05 21:28:34 -0600

    variable not really helping anyone

--- lua/user/options.lua
+++ lua/user/options.lua
@@ -1,76 +1,69 @@
 -- Neovim from Scratch:
 -- https://www.youtube.com/watch?v=hY5-Q6NxQgY&list=PLhoH5vyxr6Qq41NFL4GvhFp-WLd5xzIzZ&index=2
 
-local vo = vim.opt
-
 -- menus, modes
-vo.showmode = false     -- hide mode from cmd line (statusline has it)
-vo.pumheight = 10       -- pop up menu height
+vim.opt.showmode = false     -- hide mode from cmd line (statusline has it)
+vim.opt.pumheight = 10       -- pop up menu height
 
 -- file behavior
-vo.fileencoding = "utf-8"                  -- the encoding written to a file
-vo.backup = false                          -- creates a backup file
-vo.writebackup = false                     -- if file is being edited by another program (or was written while editing with another program), it is not allowed to be edited
-vo.swapfile = false                        -- creates a swapfile
+vim.opt.fileencoding = "utf-8"                  -- the encoding written to a file
+vim.opt.backup = false                          -- creates a backup file
+vim.opt.writebackup = false                     -- if file is being edited by another program (or was written while editing with another program), it is not allowed to be edited
+vim.opt.swapfile = false                        -- creates a swapfile
 
 -- search
-vo.ignorecase = true                       -- ignore case in search patterns
-vo.smartcase = true                        -- smart case
-vo.hlsearch = false                         -- highlight all matches on previous search pattern
-vo.incsearch = true
+vim.opt.ignorecase = true                       -- ignore case in search patterns
+vim.opt.smartcase = true                        -- smart case
+vim.opt.hlsearch = false                         -- highlight all matches on previous search pattern
+vim.opt.incsearch = true
 
 -- buffers / splits
-vo.winbar = "%f %m"                     -- filepath and modified indicator
-vo.title = true
-vo.splitbelow = true                       -- force all horizontal splits to go below current window
-vo.splitright = true                      -- force all vertical splits to go to the right of current window
--- vo.showtabline = 2                      -- always show tabs
+vim.opt.winbar = "%f %m"                     -- filepath and modified indicator
+vim.opt.title = true
+vim.opt.splitbelow = true                       -- force all horizontal splits to go below current window
+vim.opt.splitright = true                      -- force all vertical splits to go to the right of current window
 
 -- lines
-vo.cursorline = true                       -- highlight the current line
-vo.number = true
-vo.relativenumber = true
+vim.opt.cursorline = true       -- highlight the current line
+vim.opt.number = true
+vim.opt.relativenumber = true
 
 -- gutter
-vo.signcolumn = "yes"                      -- always show the sign column, otherwise it would shift the text each time
-vim.cmd "set colorcolumn=90"
-vo.numberwidth = 3                         -- set number column width to 2 {default 4}
+vim.opt.signcolumn = "yes"   -- always show the sign column, otherwise it shifts the text each time
+vim.opt.numberwidth = 3      -- width of left-side number column {default 4}
 
 -- tabstop
-vo.shiftwidth = 4                          -- the number of spaces inserted for each indentation
-vo.tabstop = 4                             -- insert n spaces for a tab
-vo.expandtab = true                        -- convert tabs to spaces
-vo.smarttab = true
-vo.smartindent = true                      -- make indenting smarter again
-vo.autoindent = true
+vim.opt.shiftwidth = 4                          -- the number of spaces inserted for each indentation
+vim.opt.tabstop = 4                             -- insert n spaces for a tab
+vim.opt.expandtab = true                        -- convert tabs to spaces
+vim.opt.smarttab = true
+vim.opt.smartindent = true                      -- make indenting smarter again
+vim.opt.autoindent = true
 
 -- wrap
-vo.wrap = false                            -- display lines as one long line
+vim.opt.colorcolumn = "90"                      -- string for some reason
+vim.opt.wrap = false                            -- display lines as one long line
 vim.cmd "set whichwrap+=<,>,[,],h,l"
 vim.cmd "set backspace=indent,eol,start"
 
 -- scrolling / scanning
-vo.scrolloff = 6
-vo.sidescrolloff = 8
+vim.opt.scrolloff = 6
+vim.opt.sidescrolloff = 8
 
 -- interactivity
-vo.mouse = "a"
--- vo.paste = true 			               -- setting paste in config creates problems??
--- vo.clipboard = "unnamedplus"
+vim.opt.mouse = "a"
+-- vim.opt.paste = true 			  -- setting paste in config creates problems??
 vim.cmd "set clipboard+=unnamedplus"
 
 -- maps and completions
-vo.timeoutlen = 1000                       -- time to wait for a mapped sequence to complete (in milliseconds)
-vo.updatetime = 300                        -- faster completion (4000ms default)
+vim.opt.timeoutlen = 1000                       -- time to wait for a mapped sequence to complete (in milliseconds)
+vim.opt.updatetime = 300                        -- faster completion (4000ms default)
 
 -- undo
-vo.undodir = ".undo"
-vo.undofile = true
+vim.opt.undodir = ".undo"
+vim.opt.undofile = true
 
 -- colors, fonts
 vim.cmd "highlight Comment cterm=italic gui=italic"
-vo.termguicolors = true
-
--- move me
--- vo.shortmess:append "c"
+vim.opt.termguicolors = true
 
