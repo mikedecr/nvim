96c86b0302e7b737ac5039d6b1d056247dc4202d
Michael DeCrescenzo <michaeldecrescenzo@Michaels-MacBook-Pro.local>
2023-04-23 17:54:38 -0500

    rename folder

---
+++ lua/user/nav/init.lua
@@ -0,0 +1,9 @@
+-- ::::::::::::::::::::::::::::::::::::::::::::::::::::
+-- ::::  navigation-related packages and settings  ::::
+-- ::::::::::::::::::::::::::::::::::::::::::::::::::::
+
+require('user.nav.telescope') -- fuzzy finder
+require('user.nav.bufjump')   -- previous/next buffer
+require('user.nav.harpoon')   -- shortlist of important files
+require('user.nav.neo_tree')  -- file tree navigation
+require('user.nav.project')   -- project root detection / change dir
