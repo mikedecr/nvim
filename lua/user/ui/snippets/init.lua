b5fd13f64ce9db20c788aba85475e75828f58ef1
Michael DeCrescenzo <mdecrescenzo@drwholdings.com>
2023-06-26 13:02:47 -0500

    snips: separate maps, comments, language snips

--- lua/user/ui/snippets/init.lua
+++ lua/user/ui/snippets/init.lua
@@ -1,36 +1,10 @@
 local snip_ok, luasnip = pcall(require, "luasnip")
 if not snip_ok then vim.notify("failed: luasnip"); return; end
 
--- :::: global snippet behaviors ::::
-
-local keymap = vim.keymap.set
-local opts = { noremap = true, silent = true }
-
--- jump forward in snip:
-keymap({"i", "s"}, "<c-l>", "<cmd>lua require'luasnip'.jump(1)<CR>", opts)
--- jump backward:
-keymap({"i", "s"}, "<c-h>", "<cmd>lua require'luasnip'.jump(-1)<CR>", opts)
-
-
 -- :::: Other things ::::
 
+require("user.ui.snippets.keymaps")
+require("user.ui.snippets.languages")
 require("user.ui.snippets.comments")
-
-
-local make_snip = luasnip.parser.parse_snippet
-
--- TODO: async def x(y) should be a variable :)
-luasnip.add_snippets(nil, {
-    python = {
-        make_snip("nf", "@NodeFactory\nasync def $1(ctx$2):\n$0", opts),
-        make_snip("async", "async def $1($2):\n$0", opts),
-        make_snip("mm", "@defmulti", opts),
-        make_snip("dm", "@defmethod($1, $2)\n$0", opts),
-        make_snip('rew', 'return await evaluate($0)', opts),
-    },
-    pandoc = {
-        make_snip('block', '```$1\n$0\n```', opts)
-    }
-})
 
 
---
+++ lua/user/ui/snippets/languages.lua
@@ -0,0 +1,33 @@
+-- ::: language-specific keymaps ::: --
+
+local snip_ok, luasnip = pcall(require, "luasnip")
+if not snip_ok then
+    vim.notify("failed: luasnip")
+    return
+end
+
+
+local make_snip = luasnip.parser.parse_snippet
+local opts = { noremap = true, silent = true }
+
+luasnip.add_snippets(nil, {
+    python = {
+        -- DRW
+        make_snip("nf", "@NodeFactory\nasync def $1(ctx$2):\n$0", opts),
+        make_snip("async", "async def $1($2):\n$0", opts),
+        make_snip("mm", "@defmulti", opts),
+        make_snip("dm", "@defmethod($1, $2)\n$0", opts),
+        make_snip('rew', 'return await evaluate($0)', opts),
+        make_snip('md', 'users.mdecrescenzo.$0'),
+        make_snip('mdm', 'users.mdecrescenzo.maps$0'),
+        make_snip('dsl', 'import clue.datasets_loader as dsl')
+    },
+    pandoc = {
+        make_snip('block', '```$1\n$0\n```', opts),
+        make_snip('box', '- [ ] $0')
+    }
+})
+
+
+
+
