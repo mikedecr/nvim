d79e92ba97b220c2de2e423b03648b0ad07d4af0
Michael DeCrescenzo <mdecrescenzo@drwholdings.com>
2023-07-03 09:38:57 -0500

    update python/drw snippets

--- lua/user/ui/snippets/languages.lua
+++ lua/user/ui/snippets/languages.lua
@@ -13,11 +13,14 @@ local opts = { noremap = true, silent = true }
 luasnip.add_snippets(nil, {
     python = {
         -- DRW
+        make_snip("cm", "import clue.maps as cm\n$0", opts),
+        make_snip("cv", "import clue.views as cv\n$0", opts),
+        make_snip("mex", "from clue.m_expr import $0", opts),
         make_snip("nf", "@NodeFactory\nasync def $1(ctx$2):\n$0", opts),
         make_snip("async", "async def $1($2):\n$0", opts),
         make_snip("mm", "@defmulti", opts),
         make_snip("dm", "@defmethod($1, $2)\n$0", opts),
-        make_snip('rew', 'return await evaluate($0)', opts),
+        make_snip('rew', 'return await evaluate(ctx, $0)', opts),
         make_snip('md', 'users.mdecrescenzo.$0'),
         make_snip('mdm', 'users.mdecrescenzo.maps$0'),
         make_snip('dsl', 'import clue.datasets_loader as dsl')
