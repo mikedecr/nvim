499efa74dbbbc74f5a335c71e50e5624361b6f1a
mikedecr <mgdecrescenzo@gmail.com>
2023-09-09 18:51:31 -0500

    trivial

--- lua/user/visual/winsep.lua
+++ lua/user/visual/winsep.lua
@@ -1,5 +1,7 @@
+-- adds highlight border to current buffer / window
+
 local ok, winsep = pcall(require, 'colorful-winsep')
-if not ok then vim.notify('winsep fails'); return; end
+if not ok then vim.notify('winsep fails') return end
 
 winsep.setup()
 
