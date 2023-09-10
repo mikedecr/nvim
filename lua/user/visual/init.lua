c9412b5519b9cccf4bb0001494f650d55fbef626
mikedecr <mgdecrescenzo@gmail.com>
2023-09-09 19:32:41 -0500

    comment winsep

--- lua/user/visual/init.lua
+++ lua/user/visual/init.lua
@@ -2,6 +2,6 @@ require('user.visual.alpha')
 require('user.visual.colors')
 require('user.visual.gitsigns')
 require('user.visual.lualine')
-require('user.visual.winsep')
+-- require('user.visual.winsep')
 require('user.visual.zen')
 
