f14515c5e6d09630890559b3e2e9e29ad2adc109
mikedecr <mgdecrescenzo@gmail.com>
2023-09-09 19:51:23 -0500

    [experimental] no harpoon

--- lua/user/nav/init.lua
+++ lua/user/nav/init.lua
@@ -3,7 +3,8 @@
 -- ::::::::::::::::::::::::::::::::::::::::::::::::::::
 
 require('user.nav.telescope') -- fuzzy finder
--- require('user.nav.bufjump')   -- previous/next buffer
-require('user.nav.harpoon')   -- shortlist of important files
 require('user.nav.neo_tree')  -- file tree navigation
-require('user.nav.project')   -- project root detection / change dir
+require('user.nav.project')   -- project root detection / change dir / tele pick
+-- require('user.nav.bufjump')   -- previous/next buffer
+-- require('user.nav.harpoon')   -- shortlist of important files
+
