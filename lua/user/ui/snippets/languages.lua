69ea85e4583fa5b4bb8af53de417f2f9dd5d0941
Michael DeCrescenzo <mdecrescenzo@drwholdings.com>
2023-11-06 08:54:47 -0600

    reorder snip list

--- lua/user/ui/snippets/languages.lua
+++ lua/user/ui/snippets/languages.lua
@@ -13,19 +13,22 @@ local opts = { noremap = true, silent = true }
 luasnip.add_snippets(nil, {
     python = {
         -- DRW
-        make_snip("cm", "import clue.maps as cm\n$0", opts),
-        make_snip("chl", "import clue.high_level as chl\n$0", opts),
-        make_snip("cv", "import clue.views as cv\n$0", opts),
-        make_snip("vd", "import clue.visidata as vd\n$0", opts),
-        make_snip("mex", "from clue.m_expr import evaluate$0", opts),
-        make_snip("nf", "@NodeFactory\nasync def $1(ctx$2):\n$0", opts),
         make_snip("async", "async def $1($2):\n$0", opts),
         make_snip("mm", "@defmulti", opts),
         make_snip("dm", "@defmethod($1, $2)\n$0", opts),
+        make_snip("nf", "@NodeFactory\nasync def $1(ctx$2):\n$0", opts),
+        make_snip("mnf", "@MergedNodeFactory\n$0", opts),
+        make_snip("giv", "gather_into_view", opts),
+        make_snip("gid", "gather_into_dict", opts),
+        make_snip("cm", "import clue.maps as cm\n$0", opts),
+        make_snip("chl", "import clue.high_level as chl\n$0", opts),
+        make_snip('dsl', 'import clue.datasets_loader as dsl'),
+        make_snip("cv", "import clue.views as cv\n$0", opts),
+        make_snip("mex", "from clue.m_expr import evaluate$0", opts),
         make_snip('rew', 'return await evaluate(ctx, $0)', opts),
+        make_snip("vd", "import clue.visidata as vd\n$0", opts),
         make_snip('md', 'users.mdecrescenzo.$0'),
-        make_snip('mdm', 'users.mdecrescenzo.maps$0'),
-        make_snip('dsl', 'import clue.datasets_loader as dsl')
+        make_snip('mdm', 'users.mdecrescenzo.maps$0')
     },
     pandoc = {
         make_snip('block', '```$1\n$0\n```', opts),
