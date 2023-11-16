49855de189b3a1063b8794b9cc62b7637b95f3a5
Mike DeCrescenzo <mdecrescenzo@chilab1.rgmadvisors.com>
2023-11-16 12:59:32 -0600

    indentation only

--- lua/user/lazy.lua
+++ lua/user/lazy.lua
@@ -1,14 +1,15 @@
 -- bootstrap lazy installation
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
-  vim.fn.system({
-    "git",
-    "clone",
-    "--filter=blob:none",
-    "https://github.com/folke/lazy.nvim.git",
-    "--branch=stable", -- latest stable release
-    lazypath,
-  })
+    vim.notify("bootstrapping Lazy.nvim")
+    vim.fn.system({
+        "git",
+        "clone",
+        "--filter=blob:none",
+        "https://github.com/folke/lazy.nvim.git",
+        "--branch=stable", -- latest stable release
+        lazypath,
+    })
 end
 vim.opt.rtp:prepend(lazypath)
 
