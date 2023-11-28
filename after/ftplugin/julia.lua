71e6be2b4fb1fc556ebb03d03d1b739820634287
Michael DeCrescenzo <mdecrescenzo@drwholdings.com>
2023-11-28 12:24:24 -0600

    revise julia maps

--- after/ftplugin/julia.lua
+++ after/ftplugin/julia.lua
@@ -4,6 +4,8 @@ local keymap = vim.keymap.set
 local opts = { noremap = true, silent = true, buffer = 0 }
 
 keymap("i", "<C-.>", "∘", opts)   -- assignment
-keymap("i", "<C-l>", "->", opts)   -- assignment
-keymap("i", "<C-k>", "=>", opts)   -- assignment
+keymap("i", "<C-->", "->", opts)   -- assignment
+keymap("i", "<C-=>", "=>", opts)   -- assignment
+keymap("i", "<C-;>", "<:", opts)   -- assignment
+keymap("i", "<C-,>", "|>", opts)   -- assignment
 
