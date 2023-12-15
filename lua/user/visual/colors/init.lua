b973a41b32424ae8ce4615e6c2e1e862ad798f6e
Michael DeCrescenzo <mdecrescenzo@drwholdings.com>
2023-12-15 10:02:22 -0600

    back to nord colors

--- lua/user/visual/colors/init.lua
+++ lua/user/visual/colors/init.lua
@@ -1,5 +1,5 @@
--- require 'user.visual.colors.nightfox'
-require 'user.visual.colors.solarized_osaka'
+require 'user.visual.colors.nightfox'
+-- require 'user.visual.colors.solarized_osaka'
 -- vim.cmd "colorscheme rose-pine-main"
 -- vim.cmd "colorscheme seoul256"
 -- vim.cmd "colorscheme noctis_minimus"
