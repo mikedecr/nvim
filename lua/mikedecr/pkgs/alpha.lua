82cb25eb7ad80ea6e6ee57c6717be6209597e33f
Michael DeCrescenzo <mdecrescenzo@drwholdings.com>
2024-04-12 18:22:36 +0000

    fix alpha buttons

--- lua/mikedecr/pkgs/alpha.lua
+++ lua/mikedecr/pkgs/alpha.lua
@@ -21,16 +21,17 @@ return {
         local alpha = require("alpha")
         local dash = require("alpha.themes.dashboard")
         dash.section.header.val = bold_mikedecr
-        -- dash.section.buttons.val = {
-        --     dashboard.button("f", "  file", ":Telescope find_files <CR>"),
-        --     dashboard.button("n", "  new", ":ene <BAR> startinsert <CR>"),
-        --     dashboard.button("r", "  recent", ":Telescope oldfiles <CR>"),
-        --     dashboard.button("t", "  search", ":Telescope live_grep <CR>"),
-        --     dashboard.button("p", "  project", ":Telescope projects <CR>"),
-        --     -- dashboard.button("c", "  configure", ":e ~/.config/nvim/init.lua <CR>"),
-        --     dashboard.button("l", "  lazy", ":Lazy<CR>"),
-        --     dashboard.button("q", "  quit", ":qa<CR>"),
-        -- }
+        dash.section.buttons.val = {
+            dash.button("f", "  file", ":Telescope find_files <CR>"),
+            dash.button("n", "  new", ":ene <BAR> startinsert <CR>"),
+            dash.button("r", "  recent", ":Telescope oldfiles <CR>"),
+            dash.button("t", "  search", ":Telescope live_grep <CR>"),
+            dash.button("p", "  project", ":Telescope projects <CR>"),
+            dash.button("c", "  configure", ":e ~/.config/nvim/init.lua <CR>"),
+            dash.button("l", "  lazy", ":Lazy<CR>"),
+            dash.button("q", "  quit", ":qa<CR>"),
+        }
+
         alpha.setup(dash.opts)
     end
 }
