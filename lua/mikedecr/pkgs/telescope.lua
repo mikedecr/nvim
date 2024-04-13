cd8e203ef0a7162e1eee5dd2894ac30480a6cc36
mikedecr <mgdecrescenzo@gmail.com>
2024-04-13 14:14:03 -0500

    style ' -> "

--- lua/mikedecr/pkgs/telescope.lua
+++ lua/mikedecr/pkgs/telescope.lua
@@ -8,24 +8,24 @@ return {
     },
 
     config = function()
-        require('telescope').setup({})
+        require("telescope").setup({})
 
-        local builtin = require('telescope.builtin')
-        vim.keymap.set('n', '<space>ff', builtin.find_files, {desc = "Tele: files"})
-        vim.keymap.set('n', '<space>gf', builtin.git_files, {desc = "Tele: git files"})
-        vim.keymap.set('n', '<space>gc', builtin.git_commits, {desc = "Tele: git commits"})
-        vim.keymap.set('n', '<space>fb', builtin.buffers, {desc = "Tele: buffers"})
-        vim.keymap.set('n', '<space>fl', builtin.live_grep, {desc = "Tele: live grep"})
+        local builtin = require("telescope.builtin")
+        vim.keymap.set("n", "<space>ff", builtin.find_files, {desc = "Tele: files"})
+        vim.keymap.set("n", "<space>gf", builtin.git_files, {desc = "Tele: git files"})
+        vim.keymap.set("n", "<space>gc", builtin.git_commits, {desc = "Tele: git commits"})
+        vim.keymap.set("n", "<space>fb", builtin.buffers, {desc = "Tele: buffers"})
+        vim.keymap.set("n", "<space>fl", builtin.live_grep, {desc = "Tele: live grep"})
         vim.keymap.set(
-            'n',
-            '<space>fw',
+            "n",
+            "<space>fw",
             function()
                 local word = vim.fn.expand("<cword>")
                 builtin.grep_string({ search = word })
             end,
             { desc = "Tele: find word" }
         )
-        vim.keymap.set('n', '<leader>vh', builtin.help_tags, { desc = "Tele: vim help tags"})
+        vim.keymap.set("n", "<leader>vh", builtin.help_tags, { desc = "Tele: vim help tags"})
     end
 }
 
