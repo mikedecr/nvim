d955e2aff42bbe9150200743fc557b2fbb18a4f6
mikedecr <mgdecrescenzo@gmail.com>
2024-04-13 10:55:42 -0500

    treesitter

---
+++ lua/mikedecr/pkgs/treesitter.lua
@@ -0,0 +1,48 @@
+return {
+    "nvim-treesitter/nvim-treesitter",
+    config = function()
+        local ts = require("nvim-treesitter")
+        ts.setup({
+            auto_install = true,
+            sync_install = false, -- only applies to ensure_installed
+            ensure_installed = {
+                'bash',
+                'gitignore',
+                'haskell',
+                'json',
+                'julia',
+                'latex',
+                'lua',
+                'markdown',
+                'markdown_inline',
+                'python',
+                'r',
+                'toml',
+                'vim',
+                'yaml'
+            },
+            indent = {
+                enable = true,
+                disable = { "yaml", "lua", "python", "r" } -- fallback is nvim-yati?
+            },
+            highlight = {
+                -- false disables entire extension
+                enable = true,
+                additional_vim_regex_highlighting = false
+                -- additional_vim_regex_highlighting = {'markdown'}, -- quarto-nvim says this is supposed 
+                                                                     -- to help something, but it makes 
+                                                                     -- quotes look really bad
+                -- disable = { "" },       -- list of language that will be disabled
+            },
+            incremental_selection = {
+                enable = true,
+                keymaps = {
+                  init_selection = "<CR>",
+                  node_incremental = "<CR>",
+                  node_decremental = "<BS>",
+                  scope_incremental = "<S-CR>",
+                },
+            }
+        })
+    end
+}
