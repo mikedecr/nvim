18655aeb39175e545087b3849057f6b429108a4e
mikedecr <mgdecrescenzo@gmail.com>
2024-04-16 21:00:47 -0500

    let's say these comments are actually bad

--- lua/mikedecr/pkgs/luasnip.lua
+++ lua/mikedecr/pkgs/luasnip.lua
@@ -1,36 +1,11 @@
 return {
     "L3MON4D3/LuaSnip",
     config = function()
-        local ls = require("luasnip")
-
-        -- snippets
         local keymap = vim.keymap.set
         local opts = { noremap = true, silent = true }
         -- jump forward/backward in snip completion menu:
         keymap({"i", "s"}, "<c-l>", "<cmd>lua require'luasnip'.jump(1)<CR>", opts)
         keymap({"i", "s"}, "<c-h>", "<cmd>lua require'luasnip'.jump(-1)<CR>", opts)
-
-        -- comment snippets
-        local text = ls.text_node
-        local snip = ls.snippet
-        local insert = ls.insert_node
-        local dashes = function(n)
-            return string.rep('-', n)
-        end
-        -- TODO
-        -- k...kill.. m..mee
-        ls.add_snippets(nil, {
-            all = {
-                snip(
-                    { trig = "comm", namr = "Big comment", dscr = "Big comment" },
-                    { text({ "# " .. dashes(50) } ),
-                      text({"", "#  "}), insert(1, "comment"),
-                      text({"",  "# " .. dashes(50), "" }),
-                    }
-                ),
-                ls.parser.parse_snippet("com", "# ----- $1 ----------$0", {}),
-            },
-        })
     end
 }
 
