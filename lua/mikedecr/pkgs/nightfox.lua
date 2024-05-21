233c1261f7fd766ddf95c24244b77a9ed7112180
Michael DeCrescenzo <mdecrescenzo@drwholdings.com>
2024-05-21 14:19:26 +0000

    terafox

--- lua/mikedecr/pkgs/nightfox.lua
+++ lua/mikedecr/pkgs/nightfox.lua
@@ -2,6 +2,6 @@ return {
     "EdenEast/nightfox.nvim",
     config = function()
         require("nightfox")
-        vim.cmd "colorscheme nordfox"
+        vim.cmd "colorscheme terafox"
     end
 }
