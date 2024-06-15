0c60474730dfe2cfbed88d13c78cf0a73a0ca754
mikedecr <mgdecrescenzo@gmail.com>
2024-06-15 14:04:46 -0500

    extend + refactor repl maps

--- lua/mikedecr/pkgs/repl.lua
+++ lua/mikedecr/pkgs/repl.lua
@@ -1,23 +1,33 @@
 return {
     "pappasam/nvim-repl",
     init = function()
+
         -- set shells
         -- for some reason python is set in pkg by default
         local SHELL = "$SHELL"
         vim.g['repl_filetype_commands'] = { python = SHELL }
         vim.g['repl_default'] = SHELL
 
-        -- repl behaviors
-        vim.g["repl_split"] = "right"
-        vim.keymap.set("n", "<space>ro", "<CMD>ReplOpen<CR>", {})
-        vim.keymap.set("n", "<space>rc", "<CMD>ReplClose<CR>", {})
+        -- repl behavior
+        vim.g["repl_split"] = "left"
 
-        -- send maps
+        -- ::: keymaps :::
+        local silent = {silent = true}
+        -- open / close
+        vim.keymap.set("n", "<space>ro", "<CMD>ReplOpen<CR>", silent)
+        vim.keymap.set("n", "<space>rc", "<CMD>ReplClose<CR>", silent)
+        vim.keymap.set("n", "<space>rt", "<CMD>ReplToggle<CR>", silent)
+        -- send visual / paragraph
         local send_visual = "<Cmd>execute 'set operatorfunc=repl#noop'<CR>:<C-u>call repl#sendvisual(visualmode())<CR>g@l<Cmd>execute 'set operatorfunc=repl#sendline'<CR>"
-        vim.keymap.set("x", "<space>rp", send_visual, {})
-        vim.keymap.set("n", "<space>rp", "vip" .. send_visual, {})
-        vim.keymap.set("x", "<space>rr", send_visual .. "]]", {})
-        vim.keymap.set("n", "<space>rr", "vip" .. send_visual .. "]]", {})
+        vim.keymap.set("x", "<space>rp", send_visual, silent)
+        vim.keymap.set("n", "<space>rp", "vip" .. send_visual, silent)
+        -- send visual / paragraph ** with step forward **
+        vim.keymap.set("x", "<space>rr", send_visual .. "]]", silent)
+        vim.keymap.set("n", "<space>rr", "vip" .. send_visual .. "]]", silent)
+        -- send line, no step fwd
+        local send_line = "<Cmd>execute 'set operatorfunc=repl#noop'<CR><Cmd>call repl#sendline()<CR>g@l<Cmd>execute 'set operatorfunc=repl#sendline'<CR>"
+        vim.keymap.set("n", "<space>sl", send_line, silent)
+
     end
 }
 
