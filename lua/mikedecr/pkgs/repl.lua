8e94137121d1d55a4e2e502ea578ebd239085336
mikedecr <mgdecrescenzo@gmail.com>
2024-06-16 14:12:10 -0500

    repl maps: open in insert mode, fix graf jump

--- lua/mikedecr/pkgs/repl.lua
+++ lua/mikedecr/pkgs/repl.lua
@@ -14,7 +14,7 @@ return {
         -- ::: keymaps :::
         local silent = {silent = true}
         -- open / close
-        vim.keymap.set("n", "<space>ro", "<CMD>ReplOpen<CR>", silent)
+        vim.keymap.set("n", "<space>ro", "<CMD>ReplOpen<CR><C-w>hi", silent)
         vim.keymap.set("n", "<space>rc", "<CMD>ReplClose<CR>", silent)
         vim.keymap.set("n", "<space>rt", "<CMD>ReplToggle<CR>", silent)
         -- send visual / paragraph
@@ -22,8 +22,8 @@ return {
         vim.keymap.set("x", "<space>rp", send_visual, silent)
         vim.keymap.set("n", "<space>rp", "vip" .. send_visual, silent)
         -- send visual / paragraph ** with step forward **
-        vim.keymap.set("x", "<space>rr", send_visual .. "]]", silent)
-        vim.keymap.set("n", "<space>rr", "vip" .. send_visual .. "]]", silent)
+        vim.keymap.set("x", "<space>rr", send_visual .. "}j", silent)
+        vim.keymap.set("n", "<space>rr", "vip" .. send_visual .. "}j", silent)
         -- send line, no step fwd
         local send_line = "<Cmd>execute 'set operatorfunc=repl#noop'<CR><Cmd>call repl#sendline()<CR>g@l<Cmd>execute 'set operatorfunc=repl#sendline'<CR>"
         vim.keymap.set("n", "<space>sl", send_line, silent)
