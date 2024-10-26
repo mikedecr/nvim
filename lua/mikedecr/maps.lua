5756ae5efcd0d4b068d612bdf01afc17d05e73f0
mikedecr <mgdecrescenzo@gmail.com>
2024-10-26 11:44:18 -0500

    chores: spacing and comments

--- lua/mikedecr/maps.lua
+++ lua/mikedecr/maps.lua
@@ -6,36 +6,36 @@ local opts = { noremap = true, silent = true }
 
 -- :::: NORMAL ::::
 
--- convenient write
 -- edit config
 vim.keymap.set("n", "<space>cc", ":e ~/.config/nvim<CR>")
 
+-- write & quit
 keymap("n", "<Space>ww", "<cmd>w<enter>", { desc = "Write file" })
 keymap("n", "<Space>qq", "<cmd>q<enter>", { desc = "Quit file" })
 keymap("n", "<Space>wq", "<cmd>wq<enter>", { desc = "Quit file" })
 keymap("n", "<Space>qf", "<cmd>q!<enter>", { desc = "Quit file" })
 
--- there is a delicate setup here.
--- This map lets me write to system (which can be fwd'd over ssh)
--- but kitty term may ask before reading from system clipboard
--- not a problem...I can manually paste text w/ OS keymap
-
+-- clipboard things [[
+--[[
+    There is a delicate setup here.
+    This map lets me write to system clipboard (which can be fwd'd over ssh)
+    but the terminal (e.g. Kitty) may ask before reading from system clipboard.
+    This is a security thing; the client doesn't know what could be in the clipboard.
+    But we can manually paste text from the operating system's keymap
+]]
 -- yank to clipboard
--- terminal may separately give you trouble trying to read clipboard
 keymap("n", "<Space>y", '"+y', opts)
 keymap("v", "<Space>y", '"+y', opts)
 
+-- SPLITS
 -- create splits
 keymap("n", "<space>sh", "<cmd>split<CR>", opts)
 keymap("n", "<space>sv", "<cmd>vsplit<CR>", opts)
-
--- Better buffer split navigation
--- these used to be sh, sj, etc. but this (regrettably) matches jupyter better
+-- split navigation
 keymap("n", "sh", "<C-w>h", opts)
 keymap("n", "sj", "<C-w>j", opts)
 keymap("n", "sk", "<C-w>k", opts)
 keymap("n", "sl", "<C-w>l", opts)
-
 -- resize splits
 keymap("n", "<C-k>", "<cmd>resize +2<cr>", {desc = "Increase window height"})
 keymap("n", "<C-j>", "<cmd>resize -2<cr>", { desc = "Decrease window height" })
@@ -45,23 +45,21 @@ keymap("n", "<C-l>", "<cmd>vertical resize +2<cr>", { desc = "Increase window wi
 -- source file
 keymap("n", "<Space>sof", "<Cmd>source %<Enter>", { desc = "Source current file" })
 
--- clear line in normal mode
+-- clear line text without deleting the line
 keymap("n", "dc", "0D", { desc = "Delete text from line" })
 
-
--- tabs
-keymap("n", "th", "<cmd>tabprev<cr>", { desc = ":tabprev" })
-keymap("n", "tl", "<cmd>tabnext<cr>", { desc = ":tabnext" })
+-- TABS
+-- creation
 keymap("n", "tr", "<cmd>tabnew<cr><cmd>terminal<cr>", { desc = "New tab with terminal buffer" })
 keymap("n", "tn", "<cmd>tabnew<cr>", { desc = "New tab" })
-
+-- navigation
+keymap("n", "th", "<cmd>tabprev<cr>", { desc = ":tabprev" })
+keymap("n", "tl", "<cmd>tabnext<cr>", { desc = ":tabnext" })
 
 -- chunk selection
 -- TODO scope to filetype config
 keymap("n", "vic", "/```<cr>NjVnk", opts) -- select chunk contents
 keymap("n", "vac", "/```<cr>NVn", opts)   -- select chunk + delimiters
--- keymap("n", "vic", "V/```<CR>ok?```<CR>j", {desc = "select inside chunk"})
--- keymap("n", "vic", "V/```<CR>?```<CR>", {desc = "select inside chunk"})
 
 
 -- :::: VISUAL ::::
@@ -74,5 +72,5 @@ keymap("v", "p", '"_dP', opts)
 
 -- :::: TERMINAL ::::
 
--- exit terminal mode
+-- exit terminal mode to normal
 keymap("t", "<Esc>", "<C-\\><C-n>", { desc = "Exit terminal mode" })
