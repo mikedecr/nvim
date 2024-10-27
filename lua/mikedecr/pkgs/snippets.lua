3a4fc57758234be10faeb9359f13f29ae30cd806
mikedecr <mgdecrescenzo@gmail.com>
2024-10-26 20:21:39 -0500

    snippets: distinguish markdown from quarto

--- lua/mikedecr/pkgs/snippets.lua
+++ lua/mikedecr/pkgs/snippets.lua
@@ -4,39 +4,49 @@ return {
 
         local luasnip = require("luasnip")
 
-        -- jump forward and backward intra-snippet
-        local keymap = vim.keymap.set
+        -- next/prev position in snippet
         local opts = { noremap = true, silent = true }
-
-        keymap({"i", "s"}, "<c-l>", "<cmd>lua require'luasnip'.jump(1)<CR>", opts)
-        keymap({"i", "s"}, "<c-h>", "<cmd>lua require'luasnip'.jump(-1)<CR>", opts)
+        vim.keymap.set({"i", "s"}, "<c-l>", "<cmd>lua require'luasnip'.jump(1)<CR>", opts)
+        vim.keymap.set({"i", "s"}, "<c-h>", "<cmd>lua require'luasnip'.jump(-1)<CR>", opts)
 
         -- custom snippets
         local make_snip = luasnip.parser.parse_snippet
 
         local markdown_snips = {
             make_snip('block', '```$1\n$0\n```', opts),
+            make_snip('box', '- [ ] $0'),
+        }
+
+        local quarto_snips = {
+            -- language blocks
             make_snip('py', '```{python}\n$0\n```', opts),
-            make_snip('md', '```{=markdown}\n$0\n```', opts),
             make_snip('r', '```{r}\n$0\n```', opts),
             make_snip('jl', '```{julia}\n$0\n```', opts),
             make_snip('yp', '```\n\n```{python}\n$0', opts),
-            make_snip('box', '- [ ] $0'),
+            make_snip('md', '```{=markdown}\n$0\n```', opts),
+            -- chunk options
             make_snip('opt', '#| $0'),
             make_snip('label', '#| label: $0'),
             make_snip('echo', '#| echo: $0'),
             make_snip('include', '#| include: $0'),
             make_snip('eval', '#| eval: $0'),
         }
+        -- inject all markdown snips into quarto
+        for k, v in ipairs(markdown_snips) do
+            quarto_snips[k] = v
+        end
+
+        local python_snips = {
+            make_snip("ia", "import $1 as $0"),
+        }
 
         luasnip.add_snippets(
             nil,
             {
-                python = {
-                    make_snip("ia", "import $1 as $0"),
-                },
+                python = python_snips,
                 pandoc = markdown_snips,
-                quarto = markdown_snips
+                markdown = markdown_snips,
+                quarto = quarto_snips
             }
         )
 
