aba65bd7cefec0a95492c06fcdb47f2aaa13b570
Mike DeCrescenzo <mdecrescenzo@drwholdings.com>
2024-11-04 10:46:13 -0600

    kanagawa: brighten unselected tabs

--- lua/mikedecr/pkgs/kanagawa-paper.lua
+++ lua/mikedecr/pkgs/kanagawa-paper.lua
@@ -4,7 +4,17 @@ return {
     priority = 1000,
     opts = {},
     config = function()
-        require("kanagawa-paper")
+        local kp = require("kanagawa-paper")
+        kp.setup({
+            overrides = function(colors)
+                local m = {
+                    -- reverse selected and non-selected tab dimness
+                    TabLine = { fg = colors.theme.syn.comment, bg = colors.theme.ui.bg_p1 },
+                    TabLineSel = { fg = colors.theme.ui.special, bg = colors.theme.ui.bg_tabline }
+                }
+                return m
+            end
+        })
         vim.cmd "colorscheme kanagawa-paper"
     end
 }
