936c10d0e179a6d38453049d99f2591f55294327
Mike DeCrescenzo <mdecrescenzo@drwholdings.com>
2024-12-03 17:20:14 -0600

    telescope live grep w/ ripgrep args

--- lua/mikedecr/pkgs/telescope.lua
+++ lua/mikedecr/pkgs/telescope.lua
@@ -4,24 +4,48 @@ return {
     tag = "0.1.6",
 
     dependencies = {
-        "nvim-lua/plenary.nvim"
+        "nvim-lua/plenary.nvim",
+        "nvim-telescope/telescope-live-grep-args.nvim"
     },
 
     config = function()
-        require("telescope").setup({
+        local telescope = require("telescope")
+        local lga_actions = require("telescope-live-grep-args.actions")
+        telescope.setup({
             -- customize picker behavior
             pickers = {
                 colorscheme = {enable_preview = true}
-            }
+            },
+            extensions = {
+                live_grep_args = {
+                  auto_quoting = true, -- enable/disable auto-quoting
+                  -- define mappings, e.g.
+                  mappings = { -- extend mappings
+                    i = {
+                      ["<C-k>"] = lga_actions.quote_prompt(),
+                      ["<C-i>"] = lga_actions.quote_prompt({ postfix = " --iglob " }),
+                      -- freeze the current list and start a fuzzy search in the frozen list
+                      ["<C-space>"] = lga_actions.to_fuzzy_refine,
+                    },
+                  },
+                  -- ... also accepts theme settings, for example:
+                  -- theme = "dropdown", -- use dropdown theme
+                  -- theme = { }, -- use own theme spec
+                  -- layout_config = { mirror=true }, -- mirror preview pane
+                }
+              }
         })
-
+        -- grep w/ args
+        telescope.load_extension("live_grep_args")
+        local live_grep_args = require("telescope").extensions.live_grep_args.live_grep_args
+        -- keymaps
         local builtin = require("telescope.builtin")
         vim.keymap.set("n", "<space>ff", builtin.find_files, {desc = "Tele: files"})
         vim.keymap.set("n", "<space>fo", builtin.oldfiles, {desc = "Tele: recent files"})
         vim.keymap.set("n", "<space>gf", builtin.git_files, {desc = "Tele: git files"})
         vim.keymap.set("n", "<space>gc", builtin.git_commits, {desc = "Tele: git commits"})
         vim.keymap.set("n", "<space>fb", builtin.buffers, {desc = "Tele: buffers"})
-        vim.keymap.set("n", "<space>fl", builtin.live_grep, {desc = "Tele: live grep"})
+        vim.keymap.set("n", "<space>fl", live_grep_args, {desc = "Tele: live grep"})
         vim.keymap.set("n", "<space>fc", builtin.colorscheme, {desc = "Tele: colorscheme"})
         vim.keymap.set(
             "n",
