5129b38e8d851c0588e53ee7d923cc27b2703c98
mikedecr <mgdecrescenzo@gmail.com>
2024-12-08 10:56:47 -0600

    indirect color scheme loads

---
+++ lua/mikedecr/pkgs/_colorschemes/tokyo-night.lua
@@ -0,0 +1,6 @@
+return {
+    "imjoshellis/tokyonight.nvim",
+    config = function()
+        vim.g.tokyonight_style = "night"
+    end
+}
---
+++ lua/mikedecr/pkgs/colorschemes.lua
@@ -0,0 +1,5 @@
+-- I think this _dir is ignored during the initial load, and then this file forces it?
+return {
+    import = "mikedecr.pkgs._colorschemes"
+}
+
