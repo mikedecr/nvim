00b73d5d6ab3f5d9a5e463fda1b4af5508c6c71e
Mike DeCrescenzo <mdecrescenzo@drwholdings.com>
2025-05-08 09:53:24 -0500

    color: nordfox default

--- init.lua
+++ init.lua
@@ -1,5 +1,7 @@
 -- all configs in a named module
 -- so swapping configs is simple
 require "mikedecr"
-vim.cmd "colorscheme kanagawa-paper"
+-- vim.cmd "colorscheme kanagawa-paper"
+-- vim.cmd "colorscheme kanagawa-lotus"
+vim.cmd "colorscheme nordfox"
 
