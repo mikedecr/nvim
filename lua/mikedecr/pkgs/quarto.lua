4e130b321539e51497907b274226982c5e1ed593
Mike DeCrescenzo <mdecrescenzo@drwholdings.com>
2025-06-04 11:34:41 -0500

    quarto maps

--- lua/mikedecr/pkgs/quarto.lua
+++ lua/mikedecr/pkgs/quarto.lua
@@ -12,6 +12,12 @@ return {
                 enabled = false
             }
         })
+        local send_visual = "<Cmd>execute 'set operatorfunc=repl#noop'<CR>:<C-u>call repl#sendvisual(visualmode())<CR>g@l<Cmd>execute 'set operatorfunc=repl#sendline'<CR>"
+
+        local opts = { silent = true }
+        vim.keymap.set("n", "]c", "/```{<CR>", opts)
+        vim.keymap.set("n", "[c", "?```{<CR>", opts)
+        vim.keymap.set("n", "<space>rc", "vic" .. send_visual .. "]c", opts)
     end
   },
 }
