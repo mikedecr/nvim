a9a8317af5b528462a416dc81eaf375f2067a945
mikedecr <mgdecrescenzo@gmail.com>
2025-06-04 21:34:53 -0500

    some more color schemes

---
+++ lua/mikedecr/pkgs/_colorschemes/accent.lua
@@ -0,0 +1,3 @@
+return {
+    "Alligator/accent.vim"
+}
---
+++ lua/mikedecr/pkgs/_colorschemes/rasmus.lua
@@ -0,0 +1,10 @@
+return {
+    "kvrohit/rasmus.nvim",
+    config = function()
+        vim.g.rasmus_italic_comments = false
+        vim.g.rasmus_italic_keywords = false
+        vim.g.rasmus_italic_functions = false
+        vim.g.rasmus_italic_booleans = false
+        vim.g.rasmus_italic_variables = false
+    end
+}
