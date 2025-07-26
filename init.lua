38dbb3128e4ca0fa4e7bb3a5a17cb2cdedf81554
mikedecr <mgdecrescenzo@gmail.com>
2025-07-26 03:10:50 +0100

    no commented junk in top-level init

--- init.lua
+++ init.lua
@@ -1,7 +1,2 @@
--- all configs in a named module
--- so swapping configs is simple
 require "mikedecr"
--- vim.cmd "colorscheme kanagawa-paper"
--- vim.cmd "colorscheme kanagawa-lotus"
-vim.cmd "colorscheme nordfox"
-
+vim.cmd "colorscheme kanagawa-paper"
