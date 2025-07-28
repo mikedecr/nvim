ae447f7d2871b38ffe114c89e3bd6e37e438b515
Mike DeCrescenzo <mdecrescenzo@drwholdings.com>
2025-07-28 09:09:52 -0500

    more explicit whichkey icons

--- lua/mikedecr/pkgs/whichkey.lua
+++ lua/mikedecr/pkgs/whichkey.lua
@@ -1,4 +1,43 @@
 return {
-    "folke/which-key.nvim"
+    "folke/which-key.nvim",
+    opts = {
+        icons = {
+            mappings = false,
+            rules = false,
+            breadcrumb = ">",
+            separator = ">",
+            ellipsis = ".",
+            keys = {
+                Up = "^ ",
+                Down = "v ",
+                Left = "< ",
+                Right = "> ",
+                C = "^-",
+                M = "m-",
+                D = "dd",
+                S = "s-",
+                CR = "cr",
+                Esc = "esc ",
+                ScrollWheelDown = "v",
+                ScrollWheelUp = "^ ",
+                NL = "<nl>",
+                BS = "<bs>",
+                Space = "<spc>",
+                Tab = ">>",
+                F1 = "f1",
+                F2 = "f2",
+                F3 = "f3",
+                F4 = "f4",
+                F5 = "f5",
+                F6 = "f6",
+                F7 = "f7",
+                F8 = "f8",
+                F9 = "f9",
+                F10 = "f10",
+                F11 = "f11",
+                F12 = "f12",
+            },
+        }
+    }
 }
 
