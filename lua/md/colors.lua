7b6554ca1ce8711c9cac911b061976ea95722e98
mikedecr <mgdecrescenzo@gmail.com>
2025-08-04 19:32:04 -0500

    more color schemes

---
+++ lua/md/colors.lua
@@ -0,0 +1,6 @@
+vim.pack.add({
+    "https://github.com/sho-87/kanagawa-paper.nvim",
+    "https://github.com/EdenEast/nightfox.nvim",
+    "https://github.com/sainnhe/everforest",
+})
+vim.cmd "colorscheme everforest"
