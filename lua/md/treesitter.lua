b45aa74accce2b60642c8f49ef728b583674618f
mikedecr <mgdecrescenzo@gmail.com>
2025-08-04 19:12:12 -0500

    treesitter and hover window border

---
+++ lua/md/treesitter.lua
@@ -0,0 +1,41 @@
+vim.pack.add({
+    "https://github.com/nvim-treesitter/nvim-treesitter",
+})
+
+local ts = require("nvim-treesitter.configs")
+ts.setup({
+    auto_install = true,
+    sync_install = false, -- only applies to ensure_installed
+    ensure_installed = {
+        'bash',
+        'gitignore',
+        'haskell',
+        'json',
+        'julia',
+        'latex',  -- needs tree-sitter-cli
+        'lua',
+        'luadoc',
+        'markdown',
+        'markdown_inline',
+        'python',
+        'r',
+        'toml',
+        'vim',
+        'vimdoc',
+        'yaml'
+    },
+    indent = { enable = true },
+    highlight = {
+        enable = true,
+        additional_vim_regex_highlighting = false
+    },
+    incremental_selection = {
+        enable = true,
+        keymaps = {
+          init_selection = "<CR>",
+          node_incremental = "<CR>",
+          node_decremental = "<BS>",
+          scope_incremental = "<S-CR>",
+        },
+    }
+})
