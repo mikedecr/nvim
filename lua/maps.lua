fdb7bdff3ad03d8ce054f4bf83fe224e406d677e
mikedecr <mgdecrescenzo@gmail.com>
2025-08-17 16:28:53 -0500

    relocate maps/options, some style

--- lua/maps.lua
+++ lua/maps.lua
@@ -20,12 +20,12 @@ end
 -- :::: NORMAL ::::
 
 -- edit config
-vim.keymap.set("n", "<space>cc", ":e ~/.config/nvim/init.lua<CR>")
+vim.keymap.set("n", "<SPACE>cc", ":e ~/.config/nvim/init.lua<CR>")
 vim.keymap.set("n", "n", "nzz")
 
 -- write & quit
-keymap("n", "<Space>ww", "<cmd>w<enter>", { desc = "Write file" })
-keymap("n", "<Space>qq", "<cmd>q<enter>", { desc = "Quit file" })
+keymap("n", "<SPACE>ww", "<CMD>w<enter>", { desc = "Write file" })
+keymap("n", "<SPACE>qq", "<CMD>q<enter>", { desc = "Quit file" })
 
 -- clipboard things [[
 --[[
@@ -36,20 +36,20 @@ keymap("n", "<Space>qq", "<cmd>q<enter>", { desc = "Quit file" })
     But we can manually paste text from the operating system's keymap
 ]]
 -- yank to clipboard
-keymap("n", "<Space>y", '"+y', opts)
-keymap("v", "<Space>y", '"+y', opts)
+keymap("n", "<SPACE>y", '"+y', opts)
+keymap("v", "<SPACE>y", '"+y', opts)
 
 -- SPLITS
 -- create splits
-keymap("n", "<space>sh", "<cmd>split<CR>", opts)
-keymap("n", "<space>sv", "<cmd>vsplit<CR>", opts)
+keymap("n", "<SPACE>sh", "<CMD>split<CR>", opts)
+keymap("n", "<SPACE>sv", "<CMD>vsplit<CR>", opts)
 -- terminal in split
-local hsplit_term = "<cmd>split term://$SHELL<CR>"
-keymap("n", "<space>th", hsplit_term .. "<C-w>H", with_opts({desc = "Terminal left"}))
-keymap("n", "<space>tl", hsplit_term .. "<C-w>L", with_opts({desc = "Terminal right"}))
-keymap("n", "<space>tj", hsplit_term .. "<C-w>J", with_opts({desc = "Terminal down"}))
-keymap("n", "<space>tk", hsplit_term .. "<C-w>K", with_opts({desc = "Terminal up"}))
-keymap("n", "<space>ro", hsplit_term .. "<C-w>H<C-w>p", with_opts({desc = "Terminal left and cursor prev"}))
+local hsplit_term = "<CMD>split term://$SHELL<CR>"
+keymap("n", "<SPACE>th", hsplit_term .. "<C-w>H", with_opts({desc = "Terminal left"}))
+keymap("n", "<SPACE>tl", hsplit_term .. "<C-w>L", with_opts({desc = "Terminal right"}))
+keymap("n", "<SPACE>tj", hsplit_term .. "<C-w>J", with_opts({desc = "Terminal down"}))
+keymap("n", "<SPACE>tk", hsplit_term .. "<C-w>K", with_opts({desc = "Terminal up"}))
+keymap("n", "<SPACE>ro", hsplit_term .. "<C-w>H<C-w>p", with_opts({desc = "Terminal left and cursor prev"}))
 -- split navigation
 keymap("n", "sh", "<C-w>h", with_opts({desc = "Focus left split"}))
 keymap("n", "sj", "<C-w>j", with_opts({desc = "Focus down split"}))
@@ -57,31 +57,31 @@ keymap("n", "sk", "<C-w>k", with_opts({desc = "Focus up split"}))
 keymap("n", "sl", "<C-w>l", with_opts({desc = "Focus right split"}))
 keymap("n", "sp", "<C-w>p", with_opts({desc = "Focus previous split"}))
 -- resize splits
-keymap("n", "<C-k>", "<cmd>resize +2<cr>", {desc = "Increase window height"})
-keymap("n", "<C-j>", "<cmd>resize -2<cr>", { desc = "Decrease window height" })
-keymap("n", "<C-h>", "<cmd>vertical resize -2<cr>", { desc = "Decrease window width" })
-keymap("n", "<C-l>", "<cmd>vertical resize +2<cr>", { desc = "Increase window width" })
+keymap("n", "<C-k>", "<CMD>resize +2<CR>", {desc = "Increase window height"})
+keymap("n", "<C-j>", "<CMD>resize -2<CR>", { desc = "Decrease window height" })
+keymap("n", "<C-h>", "<CMD>vertical resize -2<CR>", { desc = "Decrease window width" })
+keymap("n", "<C-l>", "<CMD>vertical resize +2<CR>", { desc = "Increase window width" })
 
 -- source file
-keymap("n", "<Space>sof", "<Cmd>source %<Enter>", { desc = "Source current file" })
+keymap("n", "<SPACE>sof", "<CMD>source %<Enter>", { desc = "Source current file" })
 
 -- clear line text without deleting the line
 keymap("n", "dc", "0D", { desc = "Delete text from line" })
 
 -- TABS
 -- creation
-keymap("n", "tr", "<cmd>tabnew<cr><cmd>terminal<cr>", { desc = "New tab with terminal buffer" })
-keymap("n", "tn", "<cmd>tabnew<cr>", { desc = "New tab" })
+keymap("n", "<SPACE>tr", "<CMD>tabnew<CR><CMD>terminal<CR>", { desc = "New tab with terminal buffer" })
+keymap("n", "<SPACE>tn", "<CMD>tabnew<CR>", { desc = "New tab" })
 -- navigation
-keymap("n", "th", "<cmd>tabprev<cr>", { desc = ":tabprev" })
-keymap("n", "tl", "<cmd>tabnext<cr>", { desc = ":tabnext" })
-keymap("n", "tml", "<cmd>tabmove +1<cr>", { desc = ":tabmove +1" })
-keymap("n", "tmh", "<cmd>tabmove -1<cr>", { desc = ":tabmove +1" })
+keymap("n", "th", "<CMD>tabprev<CR>", { desc = ":tabprev" })
+keymap("n", "tl", "<CMD>tabnext<CR>", { desc = ":tabnext" })
+keymap("n", "tml", "<CMD>tabmove +1<CR>", { desc = ":tabmove +1" })
+keymap("n", "tmh", "<CMD>tabmove -1<CR>", { desc = ":tabmove +1" })
 
 -- chunk selection
 -- TODO scope to filetype config
-keymap("n", "vic", "/```<cr>NjVnk", opts) -- select chunk contents
-keymap("n", "vac", "/```<cr>NVn", opts)   -- select chunk + delimiters
+keymap("n", "vic", "/```<CR>NjVnk", opts) -- select chunk contents
+keymap("n", "vac", "/```<CR>NVn", opts)   -- select chunk + delimiters
 
 -- goto previous/next buffer (these are pretty easy though...)
 keymap("n", "<C-p>", "<CMD>bp<CR>", opts)
