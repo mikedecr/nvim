85568b88fee922e298d9c7a397c03f76f9a1b1a6
mikedecr <mgdecrescenzo@gmail.com>
2025-08-17 12:38:56 -0500

    vim-slime config

---
+++ lua/md/slime.lua
@@ -0,0 +1,42 @@
+-- these two should be set before the plugin loads
+vim.g.slime_target = "neovim"
+vim.g.slime_no_mappings = true
+
+vim.pack.add({
+    "https://github.com/jpalardy/vim-slime",
+})
+
+vim.g.slime_input_pid = false
+vim.g.slime_suggest_default = true
+
+vim.g.slime_menu_config = true
+vim.g.slime_neovim_ignore_unlisted = false
+
+-- options not set here are 
+-- g:slime_neovim_menu_order,
+-- g:slime_neovim_menu_delimiter,
+-- g:slime_get_jobid
+-- see the documentation above to learn about those options
+
+-- vim.g.slime_get_jobid = function()
+--     -- iterate over all buffers to find the first terminal with a valid job
+--     for _, bufnr in ipairs(vim.api.nvim_list_bufs()) do
+--         print(bufnr)
+--         if vim.api.nvim_get_option_value('buftype',{buf = bufnr}) == "terminal" then
+--             local chan = vim.api.nvim_get_option_value( "channel",{buf = bufnr,})
+--             print(chan)
+--         if chan and chan > 0 then
+--             return chan
+--         end
+--         print("no valid chan")
+--       end
+--     end
+--     return nil
+-- end
+
+
+-- called MotionSend but works with textobjects as well
+vim.keymap.set("n", "gz", "<Plug>SlimeMotionSend", { remap = true, silent = false })
+vim.keymap.set("n", "gzz", "<Plug>SlimeLineSend", { remap = true, silent = false })
+vim.keymap.set("x", "gz", "<Plug>SlimeRegionSend", { remap = true, silent = false })
+vim.keymap.set("n", "gzc", "<Plug>SlimeConfig", { remap = true, silent = false })
