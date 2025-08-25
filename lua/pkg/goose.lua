aaf751c3bf694520eb54fe7722c5b4d5a07b87b6
Mike DeCrescenzo <mdecrescenzo@drwholdings.com>
2025-08-25 09:17:56 -0500

    DRW goose: disable markdown rendering

--- lua/pkg/goose.lua
+++ lua/pkg/goose.lua
@@ -1,7 +1,7 @@
 vim.pack.add({
     "https://github.com/azorng/goose.nvim",
     "https://github.com/nvim-lua/plenary.nvim",
-    "https://github.com/MeanderingProgrammer/render-markdown.nvim",
+    -- "https://github.com/MeanderingProgrammer/render-markdown.nvim",
 })
 
 -- implicitly using ~/.config/goose config?
