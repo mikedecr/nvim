bbac622f42b07b2abbb9239761e4433dd532efd5
mikedecr <mgdecrescenzo@gmail.com>
2025-09-06 09:33:15 -0500

    colors: everviolet and menguless

--- lua/pkg/colors.lua
+++ lua/pkg/colors.lua
@@ -2,6 +2,8 @@ vim.pack.add({
     "https://github.com/sho-87/kanagawa-paper.nvim",
     "https://github.com/EdenEast/nightfox.nvim",
     "https://github.com/neanias/everforest-nvim",
+    "https://github.com/everviolet/nvim",
+    "https://github.com/zekzekus/menguless"
 })
 
 local everforest = require("everforest")
