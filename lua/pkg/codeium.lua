4b282fd3e5a91101b9eb686c41d9f5f038becfbb
Mike DeCrescenzo <mdecrescenzo@drwholdings.com>
2025-10-29 12:14:56 -0500

    DRW codeium --> windsurf cloud config / vim support

--- lua/pkg/codeium.lua
+++ lua/pkg/codeium.lua
@@ -4,30 +4,42 @@
 
 -- easily toggle between the correct config functions based on the pkg name
 
+local which_pkg = "vim"
+
 vim.pack.add({
-    "https://github.com/Exafunction/windsurf.nvim"
+    "https://github.com/Exafunction/windsurf." .. which_pkg
 })
 
-local codeium = require('codeium')
-codeium.setup({
-    enterprise_mode = true,
-    api = {
-        host = "codeium.drwholdings.com",
-        path = "/_route/api_server",
-        portal_url = "codeium.drwholdings.com",
-    },
-    enable_chat = true,
-    -- virtual chat setup
-    virtual_text = {
-        enabled = true,
-        map_keys = true,
-        key_bindings = {
-            accept = "<C-f>",
-            clear = "<C-h>"
-        },
-        filetypes = {
-            oil = false,
-            TelescopePrompt = false
+local cmd_bindings = {
+    accept = "<C-f>",
+    clear = "<C-h>",
+}
+local binding_opts = { expr = true, silent = true}
+
+-- no vim-specific?
+if which_pkg == "nvim" then
+    local codeium = require('codeium')
+    codeium.setup({
+        -- we don't have enterprice-specific config anymore?
+        -- enterprise_mode = true,
+        -- api = {
+        --     host = "codeium.drwholdings.com",
+        --     path = "/_route/api_server",
+        --     portal_url = "codeium.drwholdings.com",
+        -- },
+        enable_chat = true,
+        virtual_text = {
+            enabled = true,
+            map_keys = true,
+            key_bindings = cmd_bindings,
+            filetypes = {
+                oil = false,
+                TelescopePrompt = false
+            }
         }
-    }
-})
+    })
+end
+if which_pkg == "vim" then
+    vim.keymap.set('i', cmd_bindings["accept"], function() return vim.fn['codeium#Accept']() end, binding_opts)
+    vim.keymap.set('i', cmd_bindings["clear"],  function() return vim.fn['codeium#Clear']() end,  binding_opts)
+end
