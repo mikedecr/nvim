4a0914db2b51a6d38e834ce5a585f6a3e3778e05
mikedecr <mgdecrescenzo@gmail.com>
2026-01-18 12:43:39 -0600

    colorschemes

--- lua/pkg/colors.lua
+++ lua/pkg/colors.lua
@@ -4,10 +4,17 @@ vim.pack.add({
     "https://github.com/neanias/everforest-nvim",
     "https://github.com/everviolet/nvim",
     "https://github.com/zekzekus/menguless",
-    "https://github.com/rjshkhr/shadow.nvim"
+    "https://github.com/rjshkhr/shadow.nvim",
+    "https://gitlab.com/motaz-shokry/gruvbox.nvim",
+    -- minimal
+    "https://github.com/hardselius/warlock",
+    "https://github.com/karoliskoncevicius/distilled-vim",
+    "https://github.com/andreypopp/vim-colors-plain",
+    "https://github.com/Alligator/accent.vim",
+    "https://github.com/nvim-mini/mini.hues"
 })
 
 require("kanagawa-paper").setup({
     terminal_colors = true
 })
-vim.cmd("colorscheme kanagawa-paper-ink")
+vim.cmd("colorscheme randomhue")
