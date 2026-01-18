18ac843f8f9f87d5c10de506dd89ee9059402714
Mike DeCrescenzo <mdecrescenzo@drwholdings.com>
2026-01-18 12:44:11 -0600

    Clue snips

--- after/ftplugin/python.lua
+++ after/ftplugin/python.lua
@@ -8,6 +8,12 @@ local make_snip = luasnip.parser.parse_snippet
 local md_snips = require("pkg.luasnip")
 
 local python_snips = {
-    make_snip("ia", "import $1 as $0")
+    make_snip("ia", "import $1 as $0"),
+    make_snip("mnf", "@pc.MergedNodeFactory\nasync def $1($2):\n\t$0"),
+    make_snip("nf", "@NodeFactory\nasync def $1($2):\n\t$0"),
+    make_snip("giv", "gather_into_view"),
+    make_snip("gid", "gather_into_dict"),
+    make_snip("icm", "from clue import maps as cm"),
+    make_snip("ipc", "from plum import core as pc")
 }
 md_snips.set_language_snippets("python", python_snips)
