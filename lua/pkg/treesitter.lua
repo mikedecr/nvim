7611134c23ce72f6c14f126ac6adccc8456499cf
mikedecr <mgdecrescenzo@gmail.com>
2026-02-16 09:51:41 -0600

    new-fashioned treesitter; checkout main, module for incremental hl

--- lua/pkg/treesitter.lua
+++ lua/pkg/treesitter.lua
@@ -1,23 +1,40 @@
 vim.pack.add({
     "https://github.com/nvim-treesitter/nvim-treesitter",
+    "https://github.com/MeanderingProgrammer/treesitter-modules.nvim"
 })
 
-require("nvim-treesitter.configs").setup({
-    auto_install = true,
-    sync_install = false, -- only applies to ensure_installed
-    ensure_installed = {
-        "bash", "gitignore", "haskell", "json", "julia",
-        "latex",  -- needs tree-sitter-cli
-        "lua", "luadoc", "markdown", "markdown_inline",
-        "python", "r", "toml", "vim", "vimdoc", "yaml"
-    },
-    indent = {
-        enable = true
-    },
-    highlight = {
-        enable = true,
-        additional_vim_regex_highlighting = false
-    },
+
+local function ts_enable(buf, language)
+end
+
+vim.api.nvim_create_autocmd('FileType', {
+    group = vim.api.nvim_create_augroup('treesitter.setup', {}),
+    callback = function(args)
+        local buf = args.buf
+        local filetype = args.match
+
+        -- if we don't have a parser, notify if it is installable.
+        local language = vim.treesitter.language.get_lang(filetype) or filetype
+        local parser_installed, err = vim.treesitter.language.add(language)
+        if not parser_installed then
+            local installable = require("nvim-treesitter").get_available()
+            if vim.tbl_contains(installable, language) then
+                print(err)
+                print(language .. " parser can be installed using :TSInstall")
+            end
+            return
+        end
+
+        -- highlighting and indentation
+        vim.treesitter.start(buf, language)
+        vim.bo[buf].indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
+
+    end,
+})
+
+
+local tsm = require("treesitter-modules")
+tsm.setup({
     incremental_selection = {
         enable = true,
         keymaps = {
