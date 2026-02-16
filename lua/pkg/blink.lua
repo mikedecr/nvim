00acbdeaff0239a026fdf3063d1eba552defcbbb
mikedecr <mgdecrescenzo@gmail.com>
2026-02-16 09:52:12 -0600

    blink: formatting mostly

--- lua/pkg/blink.lua
+++ lua/pkg/blink.lua
@@ -1,8 +1,8 @@
 vim.pack.add({
-    { src="https://github.com/Saghen/blink.cmp", version=vim.version.range("1.*") },
+    { src = "https://github.com/Saghen/blink.cmp", version = vim.version.range("1.*") },
     -- compatibility w/ cmp completion sources
-    -- blink.compat==2.* for blink 1.*
-    { src="https://github.com/saghen/blink.compat", version=vim.version.range("2.*") },
+    -- blink.compat 2.* for blink 1.*
+    { src = "https://github.com/saghen/blink.compat", version = vim.version.range("2.*") },
     -- some pkgs only augment lsp sources via cmp
     'https://github.com/hrsh7th/nvim-cmp',
     "https://github.com/hrsh7th/cmp-nvim-lsp",
@@ -15,19 +15,11 @@ vim.pack.add({
 local blink = require("blink.cmp")
 blink.setup({
     sources = {
-        default = {
-            "lsp",
-            "snippets",
-            "buffer",
-            "path",
-            "supermaven",
-        },
+        default = { "lsp", "snippets", "buffer", "path", "supermaven" },
         providers = {
-            supermaven = {
-                module = "blink-cmp-supermaven"
-            },
-            cmp_nvim_lsp = { module = "cmp-nvim-lsp" }, -- delivers otter via "lsp" default source
+            supermaven = { module = "blink-cmp-supermaven" },
         },
+        cmp_nvim_lsp = { module = "cmp-nvim-lsp" },  -- delivers otter via "lsp" default source
     },
     keymap = {
         preset = "none",
@@ -37,7 +29,7 @@ blink.setup({
         ["<C-k>"] = { "select_prev", "fallback" },
         ["<C-l>"] = { "accept", "snippet_forward" },
         ["<C-h>"] = { "show_signature", "snippet_backward" },
-        ["<C-d>"] = { "show_documentation" },
+        -- ["<C-d>"] = { "show_documentation" },
         ["<C-x>"] = { "hide_documentation" },
         ["<C-n>"] = { "scroll_documentation_down" },
         ["<C-b>"] = { "scroll_documentation_up" },
@@ -49,10 +41,11 @@ blink.setup({
             }
         },
         menu = {
+            border = "single",
             draw = {
                 columns = {
                     { "kind_icon" },
-                    { "label", "label_description" },
+                    { "label" },
                     { "source_name" },
                 },
                 -- kind text/icon could go here
