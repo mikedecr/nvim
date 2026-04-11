4cba34ae7ea9bd2ad8648f698d344383365a746f
mikedecr <mgdecrescenzo@gmail.com>
2026-04-11 10:34:05 -0500

    comment abt warning msg

--- lua/pkg/blink.lua
+++ lua/pkg/blink.lua
@@ -23,7 +23,7 @@ blink.setup({
         providers = {
             supermaven = { module = "blink-cmp-supermaven" },
         },
-        cmp_nvim_lsp = { module = "cmp-nvim-lsp" },  -- delivers otter via "lsp" default source
+        cmp_nvim_lsp = { module = "cmp-nvim-lsp" },  -- delivers otter via "lsp" default source; but we get startup warning
     },
     keymap = {
         preset = "none",
